efcbcf713d8e0917199f7dd09eafaddcdffdfd07
Javier Mercedes <manuelmercedez10@gmail.com>
2022-02-25 13:42:17 -0400

    Added nest config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from './config/config';
+import { Database } from './config/database';
 
 @Module({
-    imports: [TypeOrmModule.forRoot()],
+    imports: [
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useClass: Database,
+        }),
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [config],
+        }),
+    ],
     controllers: [],
     providers: [],
 })
