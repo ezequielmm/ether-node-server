8f9c10327f05bae205fcd9d2a778e9f8771f1100
Javier Mercedes <manuelmercedez10@gmail.com>
2022-03-02 16:51:14 -0400

    Fixed seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -33,19 +33,29 @@ const characterClassesData: Prisma.CharacterClassCreateInput[] = [
                 name: `${faker.name.firstName()} ${faker.name.lastName()}`,
             },
         },
-        cards: {
-            create: {
-                name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-                description: faker.lorem.lines(3),
-                code: faker.random.alphaNumeric(10),
-                rarity: 'common',
-                cost: generateRandomNumber(1, 100),
-                type: 'attack',
-                keyword: 'exhaust',
-                coin_cost: generateRandomNumber(1, 100),
-                status: 'active',
-            },
-        },
+    },
+];
+
+const enemiesData: Prisma.EnemyCreateInput[] = [
+    {
+        name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+        type: 'beast',
+        category: 'basic',
+    },
+];
+
+const roomsData: Prisma.RoomCreateManyInput[] = [
+    {
+        player_id: faker.datatype.uuid(),
+        status: 'finished',
+    },
+    {
+        player_id: faker.datatype.uuid(),
+        status: 'canceled',
+    },
+    {
+        player_id: faker.datatype.uuid(),
+        status: 'in_progress',
     },
 ];
 
@@ -54,29 +64,12 @@ async function main() {
     for (const data of characterClassesData) {
         await prisma.characterClass.create({ data });
     }
-    await prisma.enemy.create({
-        data: {
-            name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-            type: 'beast',
-            category: 'basic',
-        },
-    });
-    await prisma.room.createMany({
-        data: [
-            {
-                player_id: faker.datatype.uuid(),
-                status: 'finished',
-            },
-            {
-                player_id: faker.datatype.uuid(),
-                status: 'canceled',
-            },
-            {
-                player_id: faker.datatype.uuid(),
-                status: 'in_progress',
-            },
-        ],
-    });
+    for (const data of enemiesData) {
+        await prisma.enemy.create({ data });
+    }
+    for (const data of roomsData) {
+        await prisma.room.create({ data });
+    }
     console.log(`Seeding finished.`);
 }
 
