008f23daf807a398865f36648f6cd8c391c6fd99
Javier Mercedes <manuelmercedez10@gmail.com>
2022-03-11 19:36:30 -0400

    Modified cards

---
+++ prisma/migrations/20220311225830_removed_unique_to_code_in_cards/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX `cards_code_key` ON `cards`;
--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CharacterClassService } from 'src/character-class/character-class.service';
 import { PrismaModule } from 'src/prisma.module';
-import { UniqueCodeOnCardsTableRule } from 'src/validators/uniqueCodeOnCardsTable.rule';
-import { UniqueNameOnCardsTableRule } from 'src/validators/uniqueNameOnCardsTable.rule';
 import { CharacterClassExistsRule } from '../validators/characterClassExists.rule';
 import { CardController } from './card.controller';
 import { CardService } from './card.service';
@@ -10,12 +8,6 @@ import { CardService } from './card.service';
 @Module({
     imports: [PrismaModule],
     controllers: [CardController],
-    providers: [
-        CardService,
-        CharacterClassService,
-        CharacterClassExistsRule,
-        UniqueNameOnCardsTableRule,
-        UniqueCodeOnCardsTableRule,
-    ],
+    providers: [CardService, CharacterClassService, CharacterClassExistsRule],
 })
 export class CardModule {}
