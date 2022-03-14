1003cd26d7360183e4e8c9650f85da9d4a1d2299
Javier Mercedes <manuelmercedez10@gmail.com>
2022-03-14 09:55:08 -0400

    Fixed expeditions

--- src/expedition/expedition.service.ts
+++ src/expedition/expedition.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CreateExpeditionDto } from './dto/createExpedition.dto';
 import { Expedition, ExpeditionDocument } from './expedition.schema';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ExpeditionService {
@@ -10,13 +12,25 @@ export class ExpeditionService {
         private model: Model<ExpeditionDocument>,
     ) {}
 
-    async createExpedition_V1(expedition: Expedition): Promise<Expedition> {
-        const newExpedition = new this.model(expedition);
+    async createExpedition_V1(
+        expedition: CreateExpeditionDto,
+    ): Promise<Expedition> {
+        const data: Expedition = {
+            ...expedition,
+            _id: uuidv4(),
+            deck: '',
+            map: '',
+            nodes: '',
+            player_state: '',
+            current_state: '',
+            status: 'in_progress',
+        };
+        const newExpedition = new this.model(data);
         return newExpedition.save();
     }
 
-    async getExpeditionByPlayerId_V1(player_id: string): Promise<Expedition[]> {
-        return await this.model.find({ player_id }).exec();
+    async getExpeditionById_V1(id: string): Promise<Expedition[]> {
+        return await this.model.find({ _id: id }).exec();
     }
 
     async playerHasAnExpedition(player_id: string): Promise<boolean> {
