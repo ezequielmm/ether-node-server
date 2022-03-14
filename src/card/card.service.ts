6f432f2aad62495e5b2fe424ec02679710aed8ec
Javier Mercedes <manuelmercedez10@gmail.com>
2022-03-14 09:29:15 -0400

    Fixed cards methods

--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -16,12 +16,20 @@ export class CardService {
         const data: Prisma.CardFindManyArgs = {
             where: {
                 active: true,
-                OR: [],
             },
             include: {
                 cardpool: true,
             },
         };
+        if (cardpool_id) Object.assign(data.where, { OR: [{ cardpool_id }] });
+        if (card_class) {
+            Object.assign(data.where, { OR: [{ class: card_class }] });
+        }
+        if (cardpool_id && card_class) {
+            Object.assign(data.where, {
+                OR: [{ class: card_class }, { cardpool_id }],
+            });
+        }
         return await this.prisma.card.findMany(data);
     }
 }
