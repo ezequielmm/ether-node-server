721cd552d218e937d0eb1140b45a8f6110ef4762
Shahram <abdul.shahram.javed@gmail.com>
2022-03-15 15:28:33 +0500

    feat: update module file

--- src/character/character.module.ts
+++ src/character/character.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma.module';
+import { UniqueNameOnCharactersTableRule } from 'src/validators/uniqueNameOnCharactersTable.rule';
 import { CharacterController } from './character.controller';
 import { CharacterService } from './character.service';
 
 @Module({
     imports: [PrismaModule],
     controllers: [CharacterController],
-    providers: [CharacterService],
+    providers: [CharacterService, UniqueNameOnCharactersTableRule],
 })
 export class CharacterModule {}
