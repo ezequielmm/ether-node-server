e32e87f410e3c53499922f8976abbce296a7adda
Javier Mercedes <manuelmercedez10@gmail.com>
2022-03-16 14:18:06 -0400

    Fixed sockets

--- src/expedition/expedition.gateway.ts
+++ src/expedition/expedition.gateway.ts
@@ -8,33 +8,57 @@ import {
     WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { ExpeditionService } from './expedition.service';
 
 @WebSocketGateway({
     cors: {
         origin: '*',
     },
-    namespace: '/v1/socket/expeditions',
+    namespace: '/v1/expeditions/socket',
 })
 export class ExpeditionGateway
     implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+    constructor(private readonly service: ExpeditionService) {}
     @WebSocketServer() server: Server;
     private logger: Logger = new Logger('ExpeditionGateway');
 
     afterInit() {
-        this.logger.log(`Socket initiated on port`);
+        this.logger.log(`Socket initiated`);
     }
 
-    handleDisconnect({ id }: Socket) {
-        this.logger.log(`Client disconnected: ${id}`);
+    handleDisconnect(client: Socket) {
+        this.logger.log(`Client disconnected: ${client.id}`);
+        client.rooms.forEach((room) => this.logger.log(room));
     }
 
-    handleConnection({ id }: Socket) {
-        this.logger.log(`Client connected: ${id}`);
+    handleConnection(client: Socket) {
+        this.logger.log(`Client connected: ${client.id}`);
+        client.rooms.forEach((room) => this.logger.log(room));
     }
 
     @SubscribeMessage('joinExpedition')
-    handleJoinExpedition(client: Socket, room: string) {
-        client.join(room);
+    async handleJoinExpedition(
+        client: Socket,
+        payload: { player_id: string; expedition_id: string },
+    ): Promise<void> {
+        const { player_id, expedition_id } = payload;
+        if (
+            await this.service.expeditionBelongsToPlayer(
+                player_id,
+                expedition_id,
+            )
+        ) {
+            client.join(expedition_id);
+            this.server.to(expedition_id).emit('joinedExpedition', {
+                status: 'room_created',
+                expedition_id,
+            });
+        } else {
+            this.server.emit('unknownExpedition', {
+                status: 'unknown_room',
+                expedition_id,
+            });
+        }
     }
 }
