b852ad26dd37fac477387d710c8da042f03e2f80
Shahram <abdul.shahram.javed@gmail.com>
2022-03-24 18:20:04 +0500

    feat: implmenet get deck by character id api

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { CardModule } from './card/card.module';
 import { CharacterClassModule } from './character-class/character-class.module';
 import { CharacterModule } from './character/character.module';
+import { DeckModule } from './deck/deck.module';
 import { EnemyModule } from './enemy/enemy.module';
 import { ExpeditionModule } from './expedition/expedition.module';
 import { PrismaModule } from './prisma.module';
@@ -21,6 +22,7 @@ import { RoomModule } from './room/room.module';
         EnemyModule,
         RoomModule,
         ExpeditionModule,
+        DeckModule,
     ],
     controllers: [AppController],
     providers: [],
