a40a8383f386adcb1a4332b5da97150c00295fcc
Javier Mercedes <manuelmercedez10@gmail.com>
2022-04-22 14:54:29 -0400

    Fixed cards

--- README.md
+++ README.md
@@ -22,4 +22,6 @@ To run the included seeders, we run `npm run seed`
 
 This project has Swagger Docs included for quick test and references under the `/api` route.
 
-To start the project in dev mode we run `npm run start:dev` and leave the console open
+To start the project in dev mode we run `npm run start:dev` and leave the console open in the background
+
+For socket connection, the socket server runs on the same port (3000 on local) for the whole project
--- prisma/data/cards.ts
+++ prisma/data/cards.ts
@@ -1,152 +1,42 @@
-import { Prisma } from '@prisma/client';
+import { Card, CardClassEnum, CardRarityEnum, Prisma } from '@prisma/client';
 import { v4 as uuidv4 } from 'uuid';
 import { faker } from '@faker-js/faker';
 import { cardpools } from './cardpools';
 
+function getRandomBetween(min: number, max: number): number {
+    max = max + 1;
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomEnumValue<T>(anEnum: T): T[keyof T] {
+    const enumValues = Object.keys(anEnum) as Array<keyof T>;
+    const randomIndex = Math.floor(Math.random() * enumValues.length);
+    const randomEnumKey = enumValues[randomIndex];
+    return anEnum[randomEnumKey];
+}
+
+const items: Card[] = [];
+
+const cardCount = 15;
+
+for (let i = 1; i <= cardCount; i++) {
+    items.push({
+        id: uuidv4(),
+        code: 'knight_attack',
+        name: faker.name.firstName(),
+        description: faker.lorem.words(20),
+        rarity: getRandomEnumValue(CardRarityEnum),
+        card_class: getRandomEnumValue(CardClassEnum),
+        type: 'attack',
+        coins_max: getRandomBetween(1, 200),
+        coins_min: getRandomBetween(1, 200),
+        energy: getRandomBetween(1, 3),
+        cardpool_id: cardpools.id,
+        active: true,
+        created_at: new Date(),
+    });
+}
+
 export const cards: Prisma.CardCreateManyArgs = {
-    data: [
-        {
-            id: uuidv4(),
-            code: 'knight_attack',
-            name: faker.name.jobTitle(),
-            description: faker.lorem.words(20),
-            rarity: 'common',
-            card_class: 'knight',
-            type: 'attack',
-            coins_max: 0,
-            coins_min: 0,
-            energy: 1,
-            cardpool_id: cardpools.id,
-        },
-        {
-            id: uuidv4(),
-            code: 'knight_attack',
-            name: faker.name.jobTitle(),
-            description: faker.lorem.words(20),
-            rarity: 'common',
-            card_class: 'knight',
-            type: 'attack',
-            coins_max: 0,
-            coins_min: 0,
-            energy: 1,
-            cardpool_id: cardpools.id,
-        },
-        {
-            id: uuidv4(),
-            code: 'knight_attack',
-            name: faker.name.jobTitle(),
-            description: faker.lorem.words(20),
-            rarity: 'common',
-            card_class: 'knight',
-            type: 'attack',
-            coins_max: 0,
-            coins_min: 0,
-            energy: 1,
-            cardpool_id: cardpools.id,
-        },
-        {
-            id: uuidv4(),
-            code: 'knight_attack',
-            name: faker.name.jobTitle(),
-            description: faker.lorem.words(20),
-            rarity: 'common',
-            card_class: 'knight',
-            type: 'attack',
-            coins_max: 0,
-            coins_min: 0,
-            energy: 1,
-            cardpool_id: cardpools.id,
-        },
-        {
-            id: uuidv4(),
-            code: 'knight_attack',
-            name: faker.name.jobTitle(),
-            description: faker.lorem.words(20),
-            rarity: 'common',
-            card_class: 'knight',
-            type: 'attack',
-            coins_max: 0,
-            coins_min: 0,
-            energy: 1,
-            cardpool_id: cardpools.id,
-        },
-        {
-            id: uuidv4(),
-            code: 'knight_attack',
-            name: faker.name.jobTitle(),
-            description: faker.lorem.words(20),
-            rarity: 'common',
-            card_class: 'knight',
-            type: 'attack',
-            coins_max: 0,
-            coins_min: 0,
-            energy: 1,
-            cardpool_id: cardpools.id,
-        },
-        {
-            id: uuidv4(),
-            code: 'knight_attack',
-            name: faker.name.jobTitle(),
-            description: faker.lorem.words(20),
-            rarity: 'common',
-            card_class: 'knight',
-            type: 'attack',
-            coins_max: 0,
-            coins_min: 0,
-            energy: 1,
-            cardpool_id: cardpools.id,
-        },
-        {
-            id: uuidv4(),
-            code: 'knight_attack',
-            name: faker.name.jobTitle(),
-            description: faker.lorem.words(20),
-            rarity: 'common',
-            card_class: 'knight',
-            type: 'attack',
-            coins_max: 0,
-            coins_min: 0,
-            energy: 1,
-            cardpool_id: cardpools.id,
-        },
-        {
-            id: uuidv4(),
-            code: 'knight_attack',
-            name: faker.name.jobTitle(),
-            description: faker.lorem.words(20),
-            rarity: 'common',
-            card_class: 'knight',
-            type: 'attack',
-            coins_max: 0,
-            coins_min: 0,
-            energy: 1,
-            cardpool_id: cardpools.id,
-        },
-        {
-            id: uuidv4(),
-            code: 'knight_attack',
-            name: faker.name.jobTitle(),
-            description: faker.lorem.words(20),
-            rarity: 'common',
-            card_class: 'knight',
-            type: 'attack',
-            coins_max: 0,
-            coins_min: 0,
-            energy: 1,
-            cardpool_id: cardpools.id,
-        },
-        {
-            id: uuidv4(),
-            code: 'knight_attack',
-            name: faker.name.jobTitle(),
-            description: faker.lorem.words(20),
-            rarity: 'common',
-            card_class: 'knight',
-            type: 'attack',
-            coins_max: 0,
-            coins_min: 0,
-            energy: 1,
-            cardpool_id: cardpools.id,
-        },
-    ],
+    data: items,
 };
