98a80424b9ee776a9b5ab22c507927df05246baf
Juan David Melo <juan.melo@elaniin.com>
2022-05-18 13:58:55 -0500

    Minor fix

--- src/game/effects/damage.effect.ts
+++ src/game/effects/damage.effect.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { BaseEffectDTO } from './dto';
 import { IBaseEffect } from './interfaces/baseEffect';
 
 @Injectable()
-export class DamageEffect implements IBaseEffect {}
+export class DamageEffect implements IBaseEffect {
+    handle: (...args: BaseEffectDTO[]) => Promise<any>;
+}
