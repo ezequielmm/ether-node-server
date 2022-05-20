6620b244ae77428312f66cea164ade8fb8895031
Richard Garcia <richardgarcia892@gmail.com>
2022-05-20 09:46:44 -0400

    Fixed Combat_Boss Step and Portal Step having more than one node

--- src/game/expedition/map/app.ts
+++ src/game/expedition/map/app.ts
@@ -4,9 +4,7 @@ import ExpeditionMap from './map/expeditionMap';
 export function generateMap() {
     const map = new ExpeditionMap();
     map.initMap();
-    console.log(map.fullCurrentMap);
     map.extendMap();
-    console.log(map.fullCurrentMap);
     return map;
 }
 
