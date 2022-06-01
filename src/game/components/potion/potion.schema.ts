3b8cbcde163211ebf1a3849853565119c7ad384e
Javier Mercedes <manuelmercedez10@gmail.com>
2022-05-31 20:14:18 -0400

    Removed faker from potion schema

--- src/game/components/potion/potion.schema.ts
+++ src/game/components/potion/potion.schema.ts
@@ -1,13 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { Factory } from 'nestjs-seeder';
-import { Faker } from '@faker-js/faker';
 
 export type PotionDocument = Potion & Document;
 
 @Schema()
 export class Potion {
-    @Factory((faker: Faker) => faker.name.findName())
     @Prop()
     name: string;
 }
