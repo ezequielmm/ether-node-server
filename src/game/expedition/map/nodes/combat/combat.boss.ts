2e54016f65c81f4b4b5c0fa1ab597d53a783b054
Richard Garcia <richardgarcia892@gmail.com>
2022-06-16 14:54:46 -0400

    first three nodes now are always combat standard, portal show up after clearing boss stage

--- src/game/expedition/map/nodes/combat/combat.boss.ts
+++ src/game/expedition/map/nodes/combat/combat.boss.ts
@@ -1,4 +1,6 @@
 import { ExpeditionMapNodeTypeEnum } from 'src/game/expedition/enums';
+import nodeFactory from '..';
+import ExpeditionMap from '../../map/expeditionMap';
 import Combat from './combat';
 
 class CombatBoss extends Combat {
@@ -11,6 +13,22 @@ class CombatBoss extends Combat {
         private_data: any,
     ) {
         super(id, act, step, type, subType, private_data);
+    }
+    public complete(expeditionMap: ExpeditionMap): void {
+        this.setComplete();
+        const portalId: number = expeditionMap.fullCurrentMap.size + 1;
+        const portal = nodeFactory(
+            portalId,
+            this.act,
+            this.step + 1,
+            ExpeditionMapNodeTypeEnum.Portal,
+            ExpeditionMapNodeTypeEnum.Portal,
+            {},
+        );
+        portal.enter.push(this.id);
+        this.exits.push(portal.id);
+        expeditionMap.fullCurrentMap.set(portalId, portal);
+        this.openExitsNodes(expeditionMap);
     }
 }
 
