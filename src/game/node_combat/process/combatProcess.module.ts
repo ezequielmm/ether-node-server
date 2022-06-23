80e8008663134e23331efe328516acc1e3bbd472
Javier Mercedes <manuelmercedez10@gmail.com>
2022-06-23 17:25:57 -0400

    Added enemiesintent messages

--- src/game/node_combat/process/combatProcess.module.ts
+++ src/game/node_combat/process/combatProcess.module.ts
@@ -2,10 +2,11 @@ import { forwardRef, Module } from '@nestjs/common';
 import { ExpeditionModule } from 'src/game/components/expedition/expedition.module';
 import { CombatActionModule } from '../actions/combat.action.module';
 import { InitCombatProcess } from './initCombat.process';
+import { SendEnemyIntentProcess } from './sendEnemyIntent.process';
 
 @Module({
     imports: [forwardRef(() => ExpeditionModule), CombatActionModule],
-    providers: [InitCombatProcess],
-    exports: [InitCombatProcess],
+    providers: [InitCombatProcess, SendEnemyIntentProcess],
+    exports: [InitCombatProcess, SendEnemyIntentProcess],
 })
 export class CombatProcessModule {}
