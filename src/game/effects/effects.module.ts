7c6b298e90e5272ad740ee4be4cb2fcce37986e3
Javier Mercedes <manuelmercedez10@gmail.com>
2022-07-01 14:12:41 -0400

    Added energy cards

--- src/game/effects/effects.module.ts
+++ src/game/effects/effects.module.ts
@@ -4,10 +4,17 @@ import { DamageEffect } from './damage.effect';
 import { DefenseEffect } from './defense.effect';
 import { DrawCardEffect } from './drawCard.effect';
 import { EffectService } from './effects.service';
+import { EnergyEffect } from './energy.effect';
 
 @Module({
     imports: [forwardRef(() => ExpeditionModule)],
-    providers: [EffectService, DamageEffect, DefenseEffect, DrawCardEffect],
+    providers: [
+        EffectService,
+        DamageEffect,
+        DefenseEffect,
+        DrawCardEffect,
+        EnergyEffect,
+    ],
     exports: [EffectService],
 })
 export class EffectModule {}
