15f7d2bf291a4939999b19aa3ba2cf39f08b9edc
Javier Mercedes <manuelmercedez10@gmail.com>
2022-07-08 17:52:29 -0400

    started change for card duplication

--- src/game/process/initExpedition.process.ts
+++ src/game/process/initExpedition.process.ts
@@ -3,6 +3,7 @@ import { CardService } from '../components/card/card.service';
 import { CharacterClassEnum } from '../components/character/character.enum';
 import { CharacterService } from '../components/character/character.service';
 import { ExpeditionStatusEnum } from '../components/expedition/expedition.enum';
+import { IExpeditionPlayerStateDeckCard } from '../components/expedition/expedition.interface';
 import { ExpeditionService } from '../components/expedition/expedition.service';
 
 interface InitExpeditionDTO {
@@ -21,13 +22,13 @@ export class InitExpeditionProcess {
     async handle(payload: InitExpeditionDTO): Promise<void> {
         const { playerId, playerName } = payload;
 
-        const cards = await this.cardService.findAll();
-
         const character = await this.characterService.findOne({
             characterClass: CharacterClassEnum.Knight,
         });
 
         const map = this.expeditionService.getMap();
+
+        const cards = await this.generatePlayerDeck();
 
         await this.expeditionService.create({
             playerId,
@@ -38,23 +39,31 @@ export class InitExpeditionProcess {
                 hpMax: character.initialHealth,
                 hpCurrent: character.initialHealth,
                 gold: character.initialGold,
-                cards: cards.map((card) => ({
-                    cardId: card.cardId,
-                    id: card._id.toString(),
-                    name: card.name,
-                    description: card.description,
-                    rarity: card.rarity,
-                    energy: card.energy,
-                    cardType: card.cardType,
-                    pool: card.pool,
-                    properties: card.properties,
-                    keywords: card.keywords,
-                    isTemporary: false,
-                    showPointer: card.showPointer,
-                })),
+                cards,
                 createdAt: new Date(),
             },
             status: ExpeditionStatusEnum.InProgress,
         });
+    }
+
+    private async generatePlayerDeck(): Promise<
+        IExpeditionPlayerStateDeckCard[]
+    > {
+        const cards = await this.cardService.findAll();
+
+        return cards.map((card) => ({
+            cardId: card.cardId,
+            id: card._id.toString(),
+            name: card.name,
+            description: card.description,
+            rarity: card.rarity,
+            energy: card.energy,
+            cardType: card.cardType,
+            pool: card.pool,
+            properties: card.properties,
+            keywords: card.keywords,
+            isTemporary: false,
+            showPointer: card.showPointer,
+        }));
     }
 }
