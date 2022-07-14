f545a48566cbd43319bb4d1c00fb6be0022614aa
Javier Mercedes <manuelmercedez10@gmail.com>
2022-07-14 16:59:17 -0400

    Added energy message at the begin of player turn

--- src/game/process/beginPlayerTurn.process.ts
+++ src/game/process/beginPlayerTurn.process.ts
@@ -66,10 +66,31 @@ export class BeginPlayerTurnProcess {
             },
         } = await this.settingsService.getSettings();
 
-        await this.expeditionService.updatePlayerEnergy({
+        const {
+            currentNode: {
+                data: {
+                    player: { energy, energyMax },
+                },
+            },
+        } = await this.expeditionService.updatePlayerEnergy({
             clientId: client.id,
             newEnergy: initial,
         });
+
+        this.logger.log(
+            `Sent message PutData to client ${client.id}: ${SWARAction.ChangeTurn}`,
+        );
+
+        client.emit(
+            'PutData',
+            JSON.stringify(
+                StandardResponse.respond({
+                    message_type: SWARMessageType.PlayerAffected,
+                    action: SWARAction.UpdateEnergy,
+                    data: [energy, energyMax],
+                }),
+            ),
+        );
 
         await this.drawCardProcess.handle({ client, cardsTotake: handSize });
         await this.expeditionService.calculateNewEnemyIntentions(client.id);
