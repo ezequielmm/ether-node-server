cad8cc698c3f17644148b08b021aa85d6d24770b
Juan David Melo Colmenares <ontimond@gmail.com>
2022-07-27 15:10:03 -0500

    Removed unused code

--- src/game/eventManager/eventManager.interface.ts
+++
@@ -1,6 +0,0 @@
-import { ActivityLog } from '../response/activityLog';
-
-export interface EventUtils {
-    activityLog: ActivityLog;
-    emitInContext: (event: string, ...args: any[]) => void;
-}
