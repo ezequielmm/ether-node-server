6c589470ad3fc2dceecca6ef5a8debd67ed1b65c
Javier Mercedes <manuelmercedez10@gmail.com>
2022-08-01 10:52:47 -0400

    Added get playe deck meessage

---
+++ src/game/action/getPlayerDeck.action.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CardRarityEnum, CardTypeEnum } from '../components/card/card.enum';
+import { ExpeditionService } from '../components/expedition/expedition.service';
+
+interface IGetPlayerDeck {
+    id: string;
+    name: string;
+    description: string;
+    rarity: CardRarityEnum;
+    energy: number;
+    cardType: CardTypeEnum;
+    isUpgraded: boolean;
+}
+
+@Injectable()
+export class GetPlayerDeckAction {
+    constructor(private readonly expeditionService: ExpeditionService) {}
+
+    async handle(clientId: string): Promise<IGetPlayerDeck[]> {
+        const {
+            playerState: { cards },
+        } = await this.expeditionService.findOne({
+            clientId,
+        });
+
+        return cards.map((card) => ({
+            id: card.id,
+            name: card.name,
+            description: card.description,
+            rarity: card.rarity,
+            energy: card.energy,
+            cardType: card.cardType,
+            isUpgraded: card.isUpgraded,
+        }));
+    }
+}
