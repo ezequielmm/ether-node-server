8ab33c1658670f90c0e99b1be1dc357df8684c9a
Your Name <you@example.com>
2022-08-19 18:31:05 +0400

    add enemies

---
+++ src/game/components/enemy/data/thornWolf.enemy.ts
@@ -0,0 +1,165 @@
+import { damageEffect } from 'src/game/effects/damage/constants';
+import { defenseEffect } from 'src/game/effects/defense/constants';
+import { CardTargetedEnum } from '../../card/card.enum';
+import {
+    EnemyTypeEnum,
+    EnemyCategoryEnum,
+    EnemySizeEnum,
+    EnemyIntentionType,
+} from '../enemy.enum';
+import { Enemy } from '../enemy.schema';
+
+export const thornWolfData: Enemy = {
+    enemyId: 11,
+    name: 'ThornWolf',
+    type: EnemyTypeEnum.Plant,
+    category: EnemyCategoryEnum.Elite,
+    size: EnemySizeEnum.Large,
+    description:
+        'Imposing wolf-cat creature with multiple eyes and sharp thorns. Mutated by the bacteria in the swamp, he is half animal, half plant and an itchy fungal slime grows on him, making him extremely irritable. He claws at knights with his massive toxic THORN CLAWS',
+    healthRange: [85, 90],
+    scripts: [
+        {
+            intentions: [
+                {
+                    type: EnemyIntentionType.Attack,
+                    target: CardTargetedEnum.Player,
+                    value: 15,
+                    effects: [
+                        {
+                            effect: damageEffect.name,
+                            target: CardTargetedEnum.Player,
+                            args: {
+                                value: 15,
+                            },
+                        },
+                    ],
+                },
+            ],
+            next: [
+                {
+                    probability: 0.5,
+                    scriptIndex: 2,
+                },
+                {
+                    probability: 0.5,
+                    scriptIndex: 3,
+                },
+            ],
+        },
+        {
+            intentions: [
+                {
+                    type: EnemyIntentionType.Attack,
+                    target: CardTargetedEnum.Player,
+                    value: 6,
+                    effects: [
+                        {
+                            effect: damageEffect.name,
+                            target: CardTargetedEnum.Player,
+                            args: {
+                                value: 6,
+                            },
+                        },
+                    ],
+                },
+                {
+                    type: EnemyIntentionType.Attack,
+                    target: CardTargetedEnum.Player,
+                    value: 6,
+                    effects: [
+                        {
+                            effect: damageEffect.name,
+                            target: CardTargetedEnum.Player,
+                            args: {
+                                value: 6,
+                            },
+                        },
+                    ],
+                },
+            ],
+            next: [
+                {
+                    probability: 0.5,
+                    scriptIndex: 2,
+                },
+                {
+                    probability: 0.5,
+                    scriptIndex: 3,
+                },
+            ],
+        },
+        {
+            intentions: [
+                {
+                    type: EnemyIntentionType.Buff,
+                    target: CardTargetedEnum.Self,
+                    value: 5,
+                    effects: [
+                        {
+                            effect: defenseEffect.name,
+                            target: CardTargetedEnum.Self,
+                            args: {
+                                value: 5,
+                            },
+                        },
+                    ],
+                },
+                {
+                    type: EnemyIntentionType.Buff,
+                    target: CardTargetedEnum.Self,
+                    value: 2,
+                    //  This effect 'Buff ' was not developed so I have added x one
+                    effects: [
+                        {
+                            effect: defenseEffect.name,
+                            target: CardTargetedEnum.Self,
+                            args: {
+                                value: 2,
+                            },
+                        },
+                    ],
+                },
+            ],
+            next: [
+                {
+                    probability: 0.5,
+                    scriptIndex: 0,
+                },
+                {
+                    probability: 0.5,
+                    scriptIndex: 1,
+                },
+            ],
+        },
+        {
+            intentions: [
+                {
+                    type: EnemyIntentionType.Defend,
+                    target: CardTargetedEnum.Self,
+                    value: 5,
+                    //  This effect 'Summon ' was not developed so I have added x one
+                    effects: [
+                        {
+                            effect: defenseEffect.name,
+                            target: CardTargetedEnum.Self,
+                            args: {
+                                value: 5,
+                            },
+                        },
+                    ],
+                },
+            ],
+            next: [
+                {
+                    probability: 0.5,
+                    scriptIndex: 0,
+                },
+                {
+                    probability: 0.5,
+                    scriptIndex: 1,
+                },
+            ],
+        },
+    ],
+};
---
+++ src/game/components/enemy/data/trapelican.enemy.ts
@@ -0,0 +1,131 @@
+import { resolve } from 'src/game/status/resolve/constants';
+import { damageEffect } from 'src/game/effects/damage/constants';
+import { defenseEffect } from 'src/game/effects/defense/constants';
+import { CardTargetedEnum } from '../../card/card.enum';
+import {
+    EnemyTypeEnum,
+    EnemyCategoryEnum,
+    EnemySizeEnum,
+    EnemyIntentionType,
+} from '../enemy.enum';
+import { Enemy } from '../enemy.schema';
+
+export const trapelicanData: Enemy = {
+    enemyId: 12,
+    name: 'Trapelican',
+    type: EnemyTypeEnum.Plant,
+    category: EnemyCategoryEnum.Basic,
+    size: EnemySizeEnum.Small,
+    description: ' ',
+    healthRange: [26, 30],
+    scripts: [
+        {
+            intentions: [
+                {
+                    type: EnemyIntentionType.Attack,
+                    target: CardTargetedEnum.Player,
+                    value: 7,
+                    effects: [
+                        {
+                            effect: damageEffect.name,
+                            target: CardTargetedEnum.Player,
+                            args: {
+                                value: 7,
+                            },
+                        },
+                    ],
+                },
+            ],
+            next: [
+                {
+                    probability: 1,
+                    scriptIndex: 2,
+                },
+            ],
+        },
+        {
+            intentions: [
+                {
+                    type: EnemyIntentionType.Attack,
+                    target: CardTargetedEnum.Self,
+                    value: 3,
+                    effects: [
+                        {
+                            effect: defenseEffect.name,
+                            target: CardTargetedEnum.Self,
+                            args: {
+                                value: 4,
+                            },
+                        },
+                    ],
+                },
+                {
+                    type: EnemyIntentionType.Defend,
+                    target: CardTargetedEnum.Player,
+                    value: 3,
+                    effects: [
+                        {
+                            effect: damageEffect.name,
+                            target: CardTargetedEnum.Player,
+                            args: {
+                                value: 3,
+                            },
+                        },
+                    ],
+                },
+            ],
+            next: [
+                {
+                    probability: 0.6,
+                    scriptIndex: 2,
+                },
+                {
+                    probability: 0.4,
+                    scriptIndex: 0,
+                },
+            ],
+        },
+        {
+            intentions: [
+                {
+                    type: EnemyIntentionType.Buff,
+                    target: CardTargetedEnum.Player,
+                    value: 1,
+                    status: [
+                        {
+                            name: resolve.name,
+                            args: {
+                                attachTo: CardTargetedEnum.Player,
+                                value: 1,
+                            },
+                        },
+                    ],
+                },
+                {
+                    type: EnemyIntentionType.Debuff,
+                    target: CardTargetedEnum.Player,
+                    value: 1,
+                    status: [
+                        {
+                            name: resolve.name,
+                            args: {
+                                attachTo: CardTargetedEnum.Self,
+                                value: 1,
+                            },
+                        },
+                    ],
+                },
+            ],
+            next: [
+                {
+                    probability: 0.7,
+                    scriptIndex: 0,
+                },
+                {
+                    probability: 0.3,
+                    scriptIndex: 1,
+                },
+            ],
+        },
+    ],
+};
