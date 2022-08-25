f6906195e466d2c43c233075463e2063675aa5d3
Javier Mercedes <manuelmercedez10@gmail.com>
2022-08-25 09:51:06 -0400

    Fixed statuses ids

--- src/game/action/getStatuses.action.ts
+++ src/game/action/getStatuses.action.ts
@@ -21,7 +21,7 @@ export class GetStatusesAction {
         // First we get the expedition to the player's id and the
         // rest of the information from the current node
         const {
-            playerId,
+            playerState: { playerId },
             currentNode: {
                 data: {
                     player: {
