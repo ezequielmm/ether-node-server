3e3360f5eb827ff9bd988e78914aea6353652112
Juan David Melo Colmenares <ontimond@gmail.com>
2022-08-25 12:32:47 -0500

    Added draw card tests

--- src/game/effects/drawCard/drawCard.effect.spec.ts
+++ src/game/effects/drawCard/drawCard.effect.spec.ts
@@ -1,11 +1,16 @@
 import { DrawCardEffect } from './drawCard.effect';
 import { Test } from '@nestjs/testing';
 import { DrawCardAction } from 'src/game/action/drawCard.action';
-import { CardTargetedEnum } from 'src/game/components/card/card.enum';
+import {
+    CardTargetedEnum,
+    CardTypeEnum,
+} from 'src/game/components/card/card.enum';
 import { ExpeditionPlayer } from 'src/game/components/player/interfaces';
 import * as MockedSocket from 'socket.io-mock';
 import { ExpeditionDocument } from 'src/game/components/expedition/expedition.schema';
 import { Context } from 'src/game/components/interfaces';
+import { SWARMessageType } from 'src/game/standardResponse/standardResponse';
+import { EnemyIntentionType } from 'src/game/components/enemy/enemy.enum';
 
 describe('DrawCardEffect', () => {
     let drawCardEffect: DrawCardEffect;
@@ -33,6 +38,17 @@ describe('DrawCardEffect', () => {
             currentNode: {
                 data: {
                     player: mockPlayer.value.combatState,
+                    enemies: [
+                        {
+                            currentScript: {
+                                intentions: [
+                                    {
+                                        type: EnemyIntentionType.Attack,
+                                    },
+                                ],
+                            },
+                        },
+                    ],
                 },
             },
         } as ExpeditionDocument,
@@ -76,6 +92,85 @@ describe('DrawCardEffect', () => {
                 useEnemiesConfusedAsCost: false,
                 checkIfEnemyIsAttacking: false,
             },
+        });
+
+        expect(mockDrawCardAction.handle).toHaveBeenCalledWith({
+            client: mockCtx.client,
+            amountToTake: 1,
+            SWARMessageTypeToSend: SWARMessageType.PlayerAffected,
+            useEnemiesConfusedAsValue: false,
+        });
+    });
+
+    it('should draw card from draw pile and use enemies that are attacking the player', async () => {
+        await drawCardEffect.handle({
+            ctx: mockCtx,
+            source: mockPlayer,
+            target: mockPlayer,
+            args: {
+                currentValue: 1,
+                initialValue: 1,
+                useAttackingEnemies: true,
+                useEnemiesConfusedAsCost: false,
+                checkIfEnemyIsAttacking: false,
+            },
+        });
+
+        expect(mockDrawCardAction.handle).toHaveBeenCalledWith({
+            client: mockCtx.client,
+            amountToTake: 2,
+            cardType: CardTypeEnum.Defend,
+            SWARMessageTypeToSend: SWARMessageType.PlayerAffected,
+            useEnemiesConfusedAsValue: false,
+        });
+    });
+
+    it('should draw card from draw pile and use enemies that are attacking the player and are confused', async () => {
+        await drawCardEffect.handle({
+            ctx: mockCtx,
+            source: mockPlayer,
+            target: mockPlayer,
+            args: {
+                currentValue: 1,
+                initialValue: 1,
+                useAttackingEnemies: true,
+                useEnemiesConfusedAsCost: true,
+                checkIfEnemyIsAttacking: false,
+            },
+        });
+
+        expect(mockDrawCardAction.handle).toHaveBeenCalledWith({
+            client: mockCtx.client,
+            amountToTake: 2,
+            cardType: CardTypeEnum.Defend,
+            SWARMessageTypeToSend: SWARMessageType.PlayerAffected,
+            useEnemiesConfusedAsValue: true,
+        });
+    });
+
+    it('should draw card from draw pile when enenmy is the target', async () => {
+        await drawCardEffect.handle({
+            ctx: mockCtx,
+            source: mockPlayer,
+            target: {
+                value: mockCtx.expedition.currentNode.data.enemies[0],
+                type: CardTargetedEnum.Enemy,
+            },
+            args: {
+                currentValue: 1,
+                initialValue: 1,
+                useAttackingEnemies: false,
+                useEnemiesConfusedAsCost: false,
+                checkIfEnemyIsAttacking: true,
+            },
+        });
+
+        expect(mockDrawCardAction.handle).toHaveBeenCalledWith({
+            client: mockCtx.client,
+            amountToTake: 1,
+            cardType: CardTypeEnum.Defend,
+            SWARMessageTypeToSend: SWARMessageType.PlayerAffected,
+            useEnemiesConfusedAsValue: false,
         });
     });
 });
