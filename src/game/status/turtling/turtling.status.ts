e32c1a157488e12c7fd62c14c0158afd6170e138
Juan David Melo Colmenares <ontimond@gmail.com>
2022-09-07 11:47:01 -0500

    Minor fix

--- src/game/status/turtling/turtling.status.ts
+++ src/game/status/turtling/turtling.status.ts
@@ -5,7 +5,6 @@ import { EnemyService } from 'src/game/components/enemy/enemy.service';
 import { Context, ExpeditionEntity } from 'src/game/components/interfaces';
 import { PlayerService } from 'src/game/components/player/player.service';
 import {
-    EVENT_BEFORE_ENEMIES_TURN_START,
     EVENT_BEFORE_ENEMIES_TURN_END,
     EVENT_BEFORE_PLAYER_TURN_END,
 } from 'src/game/constants';
