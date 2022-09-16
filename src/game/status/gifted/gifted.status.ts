6385aace14081229d5408c61b81e63cb2537065e
Juan David Melo Colmenares <ontimond@gmail.com>
2022-09-15 22:37:12 -0500

    Added combat queue to remove defense effect and gifted status

--- src/game/status/gifted/gifted.status.ts
+++ src/game/status/gifted/gifted.status.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { CombatQueueTargetEffectTypeEnum } from 'src/game/components/combatQueue/combatQueue.enum';
+import { CombatQueueService } from 'src/game/components/combatQueue/combatQueue.service';
 import { EnemyService } from 'src/game/components/enemy/enemy.service';
 import { PlayerService } from 'src/game/components/player/player.service';
 import { StatusEventDTO, StatusEventHandler } from '../interfaces';
@@ -13,22 +15,37 @@ export class GiftedStatus implements StatusEventHandler {
     constructor(
         private readonly playerService: PlayerService,
         private readonly enemyService: EnemyService,
+        private readonly combatQueueService: CombatQueueService,
     ) {}
 
     async handle(dto: StatusEventDTO<Record<string, any>>): Promise<any> {
-        const { ctx, target, status } = dto;
+        const { ctx, source, target, status } = dto;
 
+        let finalDefense: number;
         if (PlayerService.isPlayer(target)) {
-            await this.playerService.setDefense(
-                ctx,
-                target.value.combatState.defense + status.args.value,
-            );
+            finalDefense = target.value.combatState.defense + status.args.value;
+            await this.playerService.setDefense(ctx, finalDefense);
         } else if (EnemyService.isEnemy(target)) {
+            finalDefense = target.value.defense + status.args.value;
             await this.enemyService.setDefense(
                 ctx,
                 target.value.id,
-                target.value.defense + status.args.value,
+                finalDefense,
             );
         }
+
+        await this.combatQueueService.push({
+            ctx,
+            source,
+            target,
+            args: {
+                effectType: CombatQueueTargetEffectTypeEnum.Defense,
+                defenseDelta: status.args.value,
+                finalDefense,
+                healthDelta: undefined,
+                finalHealth: undefined,
+                statuses: [],
+            },
+        });
     }
 }
