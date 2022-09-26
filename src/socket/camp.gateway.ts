a6c9a6d4101ea754997320fefe5f8d8e1b6d4450
Javier Mercedes <manuelmercedez10@gmail.com>
2022-09-26 16:10:57 -0400

    Added new messages to upgrade cards

--- src/socket/camp.gateway.ts
+++ src/socket/camp.gateway.ts
@@ -1,6 +1,8 @@
 import { Logger } from '@nestjs/common';
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { randomUUID } from 'crypto';
 import { Socket } from 'socket.io';
+import { CardService } from 'src/game/components/card/card.service';
 import { ExpeditionService } from 'src/game/components/expedition/expedition.service';
 import {
     StandardResponse,
@@ -13,7 +15,10 @@ import { corsSocketSettings } from './socket.enum';
 export class CampGateway {
     private readonly logger: Logger = new Logger(CampGateway.name);
 
-    constructor(private readonly expeditionService: ExpeditionService) {}
+    constructor(
+        private readonly expeditionService: ExpeditionService,
+        private readonly cardService: CardService,
+    ) {}
 
     @SubscribeMessage('CampRecoverHealth')
     async handleRecoverHealth(client: Socket): Promise<string> {
@@ -70,6 +75,43 @@ export class CampGateway {
 
     @SubscribeMessage('CampUpgradeCard')
     async handleUpgradeCard(client: Socket, cardId: string): Promise<void> {
-        // We take the incoming card ID
+        // First we get the player deck to confirm that the card exists
+        const playerDeck = await this.expeditionService.getDeckCards({
+            clientId: client.id,
+        });
+
+        // Now we check if the card id provided by the frontend exists
+        // On the player's deck and is not already upgraded
+        const cardToUpgrade = playerDeck.find((card) => {
+            return card.id === cardId && !card.isUpgraded;
+        });
+
+        if (cardToUpgrade !== null) {
+            // is cardToUpgrade is not null, it means that the card exists and we can upgrade it
+            // Now we deeestructure the card object to get the id of the upgraded card
+            const { upgradedCardId } = cardToUpgrade;
+
+            // Now we search for it on the database and copy it to the deck
+            const newCard = await this.cardService.findById(upgradedCardId);
+
+            // Now we create the new card on the player's deck and remove the old card
+            playerDeck.push({
+                id: randomUUID(),
+                isTemporary: false,
+                ...newCard,
+            });
+
+            const newDeck = playerDeck.filter((card) => {
+                return card.id !== cardId;
+            });
+
+            // Now we update the deck on the database
+            await this.expeditionService.updatePlayerDeck({
+                clientId: client.id,
+                deck: newDeck,
+            });
+
+            // TODO: how to inform the frontend the new changes
+        }
     }
 }
