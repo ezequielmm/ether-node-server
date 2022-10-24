f179f12a4a08086696fac89a6de9684ce204b5c4
Javier Mercedes <manuelmercedez10@gmail.com>
2022-10-24 17:59:32 -0400

    Added socket messages

--- public/index.js
+++ public/index.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
-    let io = null;
+    let socket = null;
 
     $('#btnConnectToSocket').click(function () {
         const token = document.getElementById('authToken').value;
+
+        socket = io('http://localhost:3000', {
+            extraHeaders: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        socket.on('connect', function () {
+            console.log('Connected');
+        });
+
+        socket.on('disconnect', function () {
+            console.log('Disconnected');
+        });
     });
 });
