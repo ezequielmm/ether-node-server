d38748f0ecf1dee1a87774af860f100b835f30df
Javier Mercedes <manuelmercedez10@gmail.com>
2022-10-26 14:53:00 -0400

    Added tome of wisdom trinket

--- src/game/components/trinket/trinket.seeder.ts
+++ src/game/components/trinket/trinket.seeder.ts
@@ -4,6 +4,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Trinket, TrinketDocument } from './trinket.schema';
 import { StrongCoffeeTrinket } from './data/strongCoffee.trinket';
+import { TomeOfWisdomTrinket } from './data/tomeOfWisdom.trinket';
 
 @Injectable()
 export class TrinketSeeder implements Seeder {
@@ -13,7 +14,10 @@ export class TrinketSeeder implements Seeder {
     ) {}
 
     async seed(): Promise<any> {
-        return this.trinket.insertMany([StrongCoffeeTrinket]);
+        return this.trinket.insertMany([
+            StrongCoffeeTrinket,
+            TomeOfWisdomTrinket,
+        ]);
     }
 
     async drop(): Promise<any> {
