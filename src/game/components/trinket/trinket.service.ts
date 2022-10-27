89803bca5718135e6ebcd9e4850d88efb0659233
Javier Mercedes <manuelmercedez10@gmail.com>
2022-10-27 12:37:44 -0400

    Modified trinket service:

--- src/game/components/trinket/trinket.service.ts
+++ src/game/components/trinket/trinket.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { getRandomBetween, getRandomNumber } from 'src/utils';
+import { getRandomNumber } from 'src/utils';
 import { TrinketRarityEnum } from './trinket.enum';
 import { Trinket, TrinketDocument } from './trinket.schema';
 
