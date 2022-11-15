996e53974ce1422c3a03ae7e0231f6bb6af1eeb9
Armen Hakobyan <a.aleksanyan@inorain.com>
2022-11-15 19:43:17 +0400

    combine branches

--- src/game/action/getTreasureData.action.ts
+++ src/game/action/getTreasureData.action.ts
@@ -1,13 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { randomUUID } from 'crypto';
-import { CardDescriptionFormatter } from '../cardDescriptionFormatter/cardDescriptionFormatter';
-import { CardService } from '../components/card/card.service';
-import { IExpeditionPlayerStateDeckCard } from '../components/expedition/expedition.interface';
 import { ExpeditionService } from '../components/expedition/expedition.service';
-import { PotionService } from '../components/potion/potion.service';
-import { TrinketService } from '../components/trinket/trinket.service';
-import { ItemsTypeEnum } from '../merchant/merchant.enum';
-
 @Injectable()
 export class GetTreasureDataAction {
     constructor(private readonly expeditionService: ExpeditionService) {}
