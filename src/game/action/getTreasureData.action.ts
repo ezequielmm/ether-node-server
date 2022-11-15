a8a793a3038c28aabb2aaad71af8facc51b33e73
Javier Mercedes <manuelmercedez10@gmail.com>
2022-11-15 16:36:41 -0400

    Fixed treasure node

--- src/game/action/getTreasureData.action.ts
+++ src/game/action/getTreasureData.action.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { ExpeditionService } from '../components/expedition/expedition.service';
+import { TreasureInterface } from '../treasure/interfaces';
 @Injectable()
 export class GetTreasureDataAction {
     constructor(private readonly expeditionService: ExpeditionService) {}
 
-    async handle(clientId: string) {
+    async handle(clientId: string): Promise<TreasureInterface['type']> {
         const { nodeId } = await this.expeditionService.getCurrentNode({
             clientId,
         });
