a6c54c4f284a76d84fa12ed1de13d5df7ad00008
Jose Castelli <jose.castelli@toptal.com>
2022-11-17 08:58:51 -0300

    refactoring changeturn test

--- src/game/action/changeTurn.action.spec.ts
+++ src/game/action/changeTurn.action.spec.ts
@@ -1,7 +1,4 @@
-import {
-    DebugLogger,
-    IntegrationTestServer,
-} from 'src/tests/integrationTestServer';
+import { IntegrationTestServer } from 'src/tests/integrationTestServer';
 import { ServerSocketGatewayMock } from 'src/tests/serverSocketGatewayMock';
 import { CombatTurnEnum } from '../components/expedition/expedition.enum';
 import {
@@ -26,32 +23,31 @@ describe('ChangeTurnAction', () => {
     });
 
     it('handle change turn action', async () => {
-        const clientSocket = await its.addNewSocketConnection();
+        const [clientSocket, messages] = await its.addNewSocketConnection();
         const cases: (SWARMessageType.BeginTurn | SWARMessageType.EndTurn)[] = [
             SWARMessageType.EndTurn,
             SWARMessageType.BeginTurn,
         ];
 
         for (const c of cases) {
-            let putDataMessage;
-            clientSocket.on('PutData', (message) => {
-                putDataMessage = JSON.parse(message);
-            });
-
             await changeTurnAction.handle({
                 client: mockedSocketGateway.clientSocket,
                 type: c,
                 entity: CombatTurnEnum.Player,
             });
-
-            await new Promise<void>((resolve) => setTimeout(resolve, 5));
-
-            expect(putDataMessage).toBeDefined();
-            expect(putDataMessage.data).toBeDefined();
-            expect(putDataMessage.data.message_type).toBe(c);
-            expect(putDataMessage.data.action).toBe(SWARAction.ChangeTurn);
-            expect(putDataMessage.data.data).toBe(CombatTurnEnum.Player);
         }
+
+        await clientSocket.waitMessages(messages, 2);
+        expect(messages).toHaveLength(2);
+
+        cases.forEach((c, i) => {
+            const message = messages[i];
+            expect(message).toBeDefined();
+            expect(message.data).toBeDefined();
+            expect(message.data.message_type).toBe(c);
+            expect(message.data.action).toBe(SWARAction.ChangeTurn);
+            expect(message.data.data).toBe(CombatTurnEnum.Player);
+        });
     });
 
     afterAll(async () => {
