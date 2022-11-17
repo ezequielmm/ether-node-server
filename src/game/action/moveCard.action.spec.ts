d3f30c9275e4f4f5c892ad0ed916ed117bad5e42
Jose Castelli <jose.castelli@toptal.com>
2022-11-17 14:22:17 -0300

    adding more tests

---
+++ src/game/action/moveCard.action.spec.ts
@@ -0,0 +1,197 @@
+import { Enemy, EnemySchema } from '../components/enemy/enemy.schema';
+import {
+    CombatTurnEnum,
+    ExpeditionMapNodeStatusEnum,
+    ExpeditionMapNodeTypeEnum,
+    ExpeditionStatusEnum,
+} from '../components/expedition/expedition.enum';
+import {
+    Expedition,
+    ExpeditionSchema,
+} from '../components/expedition/expedition.schema';
+import { ExpeditionService } from '../components/expedition/expedition.service';
+import { PlayerService } from '../components/player/player.service';
+import { EnemyService } from '../components/enemy/enemy.service';
+import { EffectService } from '../effects/effects.service';
+import { CardPlayedAction } from './cardPlayed.action';
+import { StatusService } from '../status/status.service';
+import { CombatQueueService } from '../components/combatQueue/combatQueue.service';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { HistoryService } from '../history/history.service';
+import { DiscardCardAction } from './discardCard.action';
+import { ExhaustCardAction } from './exhaustCard.action';
+import { EndPlayerTurnProcess } from '../process/endPlayerTurn.process';
+import { ServerSocketGatewayMock } from 'src/tests/serverSocketGatewayMock';
+import { CardSeeder } from '../components/card/card.seeder';
+import { Card, CardSchema } from '../components/card/card.schema';
+import { ProviderService } from '../provider/provider.service';
+import {
+    CombatQueue,
+    CombatQueueSchema,
+} from '../components/combatQueue/combatQueue.schema';
+import { DefenseEffect } from '../effects/defense/defense.effect';
+
+import { DamageEffect } from '../effects/damage/damage.effect';
+import { GetEnergyAction } from './getEnergy.action';
+import {
+    DebugLogger,
+    IntegrationTestServer,
+} from 'src/tests/integrationTestServer';
+import { CreateCardAction } from './createCard.action';
+import { CardService } from '../components/card/card.service';
+import { MoveCardAction } from './moveCard.action';
+import { data } from '../components/card/card.data';
+import {
+    SWARAction,
+    SWARMessageType,
+} from '../standardResponse/standardResponse';
+import { DiscardAllCardsAction } from './discardAllCards.action';
+import { CardServiceMock } from 'src/tests/cardServiceMock';
+import { AttackCard } from '../components/card/data/attack.card';
+import { AutonomousWeaponCard } from '../components/card/data/autonomousWeapon.card';
+import { FullSyncAction } from './fullSync.action';
+import { GetCardPilesAction } from './getCardPiles.action';
+import { GetCurrentStepAction } from './getCurrentStep.action';
+import { GetPlayerDeckAction } from './getPlayerDeck.action';
+import { StunnedCard } from '../components/card/data/stunned.card';
+import { GetPlayerInfoAction } from './getPlayerInfo.action';
+import { GetStatusesAction } from './getStatuses.action';
+import { GetTreasureDataAction } from './getTreasureData.action';
+import { LargeChest, TrappedType } from '../treasure/treasure.enum';
+import { GetUpgradableCardsAction } from './getUpgradableCards.action';
+
+describe('DiscardCardAction', () => {
+    let its: IntegrationTestServer;
+    let mockedSocketGateway: ServerSocketGatewayMock;
+    let expeditionService: ExpeditionService;
+    let moveCardAction: MoveCardAction;
+    let cardService: CardService;
+
+    beforeAll(async () => {
+        its = new IntegrationTestServer();
+        await its.start({
+            providers: [
+                {
+                    provide: PlayerService,
+                    useValue: {},
+                },
+                {
+                    provide: EnemyService,
+                    useValue: {},
+                },
+                {
+                    provide: CardPlayedAction,
+                    useValue: {},
+                },
+                {
+                    provide: StatusService,
+                    useValue: {},
+                },
+                MoveCardAction,
+                CardService,
+                ServerSocketGatewayMock,
+                ExpeditionService,
+                GetUpgradableCardsAction,
+                CardSeeder,
+            ],
+            models: [
+                { name: Expedition.name, schema: ExpeditionSchema },
+                { name: Card.name, schema: CardSchema },
+            ],
+        });
+
+        cardService = its.getInjectable(CardService);
+
+        mockedSocketGateway = its.getInjectable(ServerSocketGatewayMock);
+        expeditionService = its.getInjectable(ExpeditionService);
+        moveCardAction = its.getInjectable(MoveCardAction);
+
+        const cardSeeder = its.getInjectable(CardSeeder);
+        await cardSeeder.seed();
+    });
+
+    it('get card piles action', async () => {
+        const [clientSocket, messages] = await its.addNewSocketConnection();
+        const clientId = clientSocket.socket.id;
+
+        const stunnedCard = await cardService.findById(StunnedCard.cardId);
+        expect(stunnedCard).toBeDefined();
+
+        await expeditionService.create({
+            clientId: clientId,
+            playerId: 0,
+            map: [],
+            playerState: undefined,
+            status: ExpeditionStatusEnum.InProgress,
+            currentNode: {
+                nodeId: 0,
+                nodeType: ExpeditionMapNodeTypeEnum.Combat,
+                completed: false,
+                showRewards: true,
+                data: {
+                    round: 0,
+                    playing: CombatTurnEnum.Player,
+                    rewards: [],
+                    enemies: [],
+                    player: {
+                        energy: 3,
+                        energyMax: 4,
+                        handSize: 1,
+                        defense: 1,
+                        hpCurrent: 10,
+                        hpMax: 10,
+                        statuses: {
+                            buff: [],
+                            debuff: [],
+                        },
+                        cards: {
+                            hand: [
+                                {
+                                    id: stunnedCard.cardId.toString(),
+                                    isTemporary: false,
+                                    ...stunnedCard,
+                                },
+                            ],
+                            draw: [],
+                            discard: [],
+                            exhausted: [],
+                        },
+                    },
+                },
+            },
+        });
+
+        const expedition = await expeditionService.findOne({ clientId });
+        expect(expedition).toBeDefined();
+
+        expect(expedition.currentNode.data.player.cards.hand).toHaveLength(1);
+        expect(expedition.currentNode.data.player.cards.discard).toHaveLength(
+            0,
+        );
+
+        await moveCardAction.handle({
+            client: mockedSocketGateway.clientSocket,
+            cardIds: [stunnedCard.cardId.toString()],
+            originPile: 'hand',
+            targetPile: 'discard',
+        });
+
+        // MOVE CARD ACTION NOT WORKING!!!! Discard pile should be one and hand 0
+        expect(expedition.currentNode.data.player.cards.hand).toHaveLength(1);
+        expect(expedition.currentNode.data.player.cards.discard).toHaveLength(
+            0,
+        );
+
+        await clientSocket.waitMessages(messages, 1);
+        expect(messages).toHaveLength(1);
+        const message = messages[0];
+        expect(message.data.action).toBe(SWARAction.MoveCard);
+        expect(message.data.data).toMatchObject([
+            { destination: 'discard', id: '501' },
+        ]);
+    });
+
+    afterAll(async () => {
+        await its.stop();
+    });
+});
