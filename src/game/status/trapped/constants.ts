181f53d3102d1de42b1a6cf541c4ee56a0f984c0
Juan David Melo Colmenares <ontimond@gmail.com>
2022-11-22 15:13:53 -0500

    Trapped status fixed

--- src/game/status/trapped/constants.ts
+++ src/game/status/trapped/constants.ts
@@ -11,7 +11,7 @@ export const trapped: StatusEffect = {
     name: 'trapped',
     type: StatusType.Buff,
     counterType: StatusCounterType.None,
-    direction: StatusDirection.Outgoing,
+    direction: StatusDirection.Incoming,
     trigger: StatusTrigger.Effect,
     effects: [damageEffect],
 };
