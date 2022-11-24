925c2d4580b170ba224915ee661ff910cf91f090
Javier Mercedes <manuelmercedez10@gmail.com>
2022-11-23 21:16:16 -0400

    Fixed rewards

--- src/socket/reward.gateway.ts
+++ src/socket/reward.gateway.ts
@@ -52,7 +52,11 @@ export class RewardGateway {
 
         let rewards: Reward[] = [];
 
-        rewards = expedition.currentNode.data.rewards;
+        if (nodeType === ExpeditionMapNodeTypeEnum.Treasure) {
+            rewards = expedition.currentNode.treasureData.rewards;
+        } else {
+            rewards = expedition.currentNode.data.rewards;
+        }
 
         if (nodeIsCompleted) {
             // Check if the node is completed
