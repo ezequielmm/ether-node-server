6f323d750ac9cb81c0f81ce90fe11685924d9551
Javier Mercedes <manuelmercedez10@gmail.com>
2022-12-26 15:49:54 -0400

    Added spawn effect and updated methods to save the game state

--- src/game/effects/spawnEnemy/spawnEnemy.effect.ts
+++ src/game/effects/spawnEnemy/spawnEnemy.effect.ts
@@ -1,17 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { forEach } from 'lodash';
+import { EnemyService } from 'src/game/components/enemy/enemy.service';
+import { ExpeditionService } from 'src/game/components/expedition/expedition.service';
 import { EffectDecorator } from '../effects.decorator';
 import { EffectDTO, EffectHandler } from '../effects.interface';
 import { spawnEnemyEffect } from './contants';
+
+export interface SpawnEnemyArgs {
+    enemiesToSpawn: number[];
+}
 
 @EffectDecorator({
     effect: spawnEnemyEffect,
 })
 @Injectable()
 export class SpawnEnemyEffect implements EffectHandler {
-    async handle(dto: EffectDTO): Promise<void> {
-        const { ctx } = dto;
+    constructor(
+        private readonly enemyService: EnemyService,
+        private readonly expeditionService: ExpeditionService,
+    ) {}
+
+    async handle(dto: EffectDTO<SpawnEnemyArgs>): Promise<void> {
+        const {
+            ctx,
+            args: { enemiesToSpawn },
+        } = dto;
 
         // First we check the enemies that we are going to spawn in
-        // during combat
+        // during combat and get their information from the database
+        const enemies = await this.enemyService.findEnemiesById(enemiesToSpawn);
+
+        // Now if we find enemies, we add them to the combat state and send a new message
+        // To the frontend so they can show the proper animations
+        if (enemies.length > 0) {
+            // Here we add the enemies to the enemies array on the current state object
+
+            forEach(enemies, (enemy) => {});
+        }
     }
 }
