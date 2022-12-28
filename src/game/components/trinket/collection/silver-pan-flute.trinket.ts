c3a08d07695aa786adce2c88211bd37b236c3056
Javier Mercedes <manuelmercedez10@gmail.com>
2022-12-28 12:10:20 -0400

    added silver pan flute

---
+++ src/game/components/trinket/collection/silver-pan-flute.trinket.ts
@@ -0,0 +1,76 @@
+import { Prop } from '@typegoose/typegoose';
+import { EVENT_AFTER_INIT_COMBAT } from 'src/game/constants';
+import { resolveStatus } from 'src/game/status/resolve/constants';
+import { StatusService } from 'src/game/status/status.service';
+import { CardTargetedEnum } from '../../card/card.enum';
+import { CombatQueueService } from '../../combatQueue/combatQueue.service';
+import { GameContext } from '../../interfaces';
+import { PlayerService } from '../../player/player.service';
+import { TrinketRarityEnum } from '../trinket.enum';
+import { Trinket } from '../trinket.schema';
+
+export class SilverPanFluteTrinket extends Trinket {
+    @Prop({ default: 46 })
+    trinketId: number;
+
+    @Prop({ default: 'Silver Pan Flute' })
+    name: string;
+
+    @Prop({ default: 'At the start of combat, gain 2 Resolve' })
+    description: string;
+
+    @Prop({ default: TrinketRarityEnum.Special })
+    rarity: TrinketRarityEnum;
+
+    @Prop({ default: 2 })
+    resolveToGain: number;
+
+    onAttach(ctx: GameContext): void {
+        ctx.events.addListener(EVENT_AFTER_INIT_COMBAT, async () => {
+            const playerService = ctx.moduleRef.get(PlayerService, {
+                strict: false,
+            });
+
+            const statusService = ctx.moduleRef.get(StatusService, {
+                strict: false,
+            });
+
+            const combatQueueService = ctx.moduleRef.get(CombatQueueService, {
+                strict: false,
+            });
+
+            const player = playerService.get(ctx);
+
+            await combatQueueService.start(ctx);
+
+            await statusService.attach({
+                ctx,
+                source: player,
+                target: player,
+                statusName: resolveStatus.name,
+                statusArgs: {
+                    value: this.resolveToGain,
+                    counter: this.resolveToGain,
+                },
+            });
+
+            await combatQueueService.pushStatuses(ctx, player, player, [
+                {
+                    name: resolveStatus.name,
+                    addedInRound: 1,
+                    sourceReference: {
+                        type: CardTargetedEnum.Player,
+                    },
+                    args: {
+                        value: this.resolveToGain,
+                        counter: this.resolveToGain,
+                    },
+                },
+            ]);
+
+            await combatQueueService.end(ctx);
+
+            await combatQueueService.deleteCombatQueueByClientId(ctx.client.id);
+        });
+    }
+}
