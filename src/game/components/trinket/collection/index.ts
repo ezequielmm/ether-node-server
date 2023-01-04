cfc1e2609f2b269e9fb88544457d0eddfc100441
Armen Hakobyan <a.aleksanyan@inorain.com>
2023-01-04 12:11:13 +0400

    Goblet_trinket

--- src/game/components/trinket/collection/index.ts
+++ src/game/components/trinket/collection/index.ts
@@ -13,3 +13,5 @@ export { WhistleTrinket } from './whistle.trinket';
 export { WhistleUpgradedTrinket } from './whistle-upgraded.trinket';
 export { SilverPanFluteTrinket } from './silver-pan-flute.trinket';
 export { GoldPanFluteTrinket } from './gold-pan-flute.trinket';
+export { GobletUpgradedTrinket } from './goblet-upgraded.trinket';
+export { GobletTrinket } from './goblet.trinket';
