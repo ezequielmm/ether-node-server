40c230f2f6f1064c893b0b9d229e1d59f5a7cc03
Javier Mercedes <manuelmercedez10@gmail.com>
2023-01-13 17:39:52 -0400

    Added texts for effects

--- src/game/effects/EffectGenerator.ts
+++ src/game/effects/EffectGenerator.ts
@@ -1,10 +1,16 @@
+import { addCardEffect } from './addCard/contants';
 import { damageEffect } from './damage/constants';
+import { defenseEffect } from './defense/constants';
 
 export class EffectGenerator {
     public static generateDescription(name: string, value?: number): string {
         switch (name) {
             case damageEffect.name:
                 return `This card will attack for ${value} damage`;
+            case defenseEffect.name:
+                return `This card will add ${value} defense points to the player`;
+            case addCardEffect.name:
+                return `This will add ${value} cards to the hand`;
             default:
                 return 'Unknown effect';
         }
