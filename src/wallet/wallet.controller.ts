ea448a1b2314e38da933f78a5ca29c6ca8bb07d8
Javier Emmanuel Mercedes <manuelmercedez10@gmail.com>
2023-01-27 11:35:24 -0400

    Added wallet endpoint to openAPI

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { WalletService } from './wallet.service';
 
 @Controller('wallets')
 export class WalletController {
     constructor(private walletService: WalletService) {}
 
+    @ApiOperation({
+        summary: 'Get NFT',
+    })
     @Get(':id')
-    getMe(@Param('id') walletId: string) {
+    getTokenList(@Param('id') walletId: string) {
         return this.walletService.getTokenIdList(walletId);
     }
 }
