db2084501d9798eadcfd5554e35ac4eb404cac7d
tonygrahn <118852733+tonygrahn@users.noreply.github.com>
2023-03-10 17:14:06 -0800

    Replicate and use nft moralis service

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import { ConfigService } from '@nestjs/config';
+import NFTService from '../nft-library/services/nft_service';
 
 @Injectable()
 export class WalletService {
@@ -12,6 +13,22 @@ export class WalletService {
     ) {}
 
     async getTokenIdList(walletId: string, contractId): Promise<string[]> {
+        // the chain where are deployed the smart contracts
+        const chain = 5;
+        // The wallet address from he user
+        const wallet = '0xa10f15b66a2e05c4e376f8bfc35ae662438153be';
+        // The contracts to filter from all the user collections
+        const contracts = [
+            '0xF0aA34f832c34b32478B8D9696DC8Ad1c8065D2d',
+            '0x80e2109a826148b9b1a41b0958ca53a4cdc64b70',
+        ];
+        const nfts = await NFTService.listByContracts(chain, wallet, contracts);
+
+        return nfts;
+        
+        
+        /*
+        
         const domain = this.configService.get<string>('NFT_SERVICE_URL');
         const contract_id = this.configService.get<string>(
             'NFT_SERVICE_CONTRACT_ID',
@@ -46,5 +63,6 @@ export class WalletService {
         }
 
         return tokenArray;
+        */
     }
 }
