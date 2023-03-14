b076cc2dae7c4f901af5ba53a41daf13d5d82078
Juan David Melo Colmenares <ontimond@gmail.com>
2023-03-14 16:00:09 -0500

    Commit message: Add shuffling logic for cards drawn from deck

--- src/game/effects/addCard/addCard.effect.ts
+++ src/game/effects/addCard/addCard.effect.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { randomUUID } from 'crypto';
+import { shuffle } from 'lodash';
 import { Card } from 'src/game/components/card/card.schema';
 import { CardService } from 'src/game/components/card/card.service';
 import { IExpeditionPlayerStateDeckCard } from 'src/game/components/expedition/expedition.interface';
-import { ExpeditionService } from 'src/game/components/expedition/expedition.service';
+import { Expedition } from 'src/game/components/expedition/expedition.schema';
 import { PlayerService } from 'src/game/components/player/player.service';
 import {
     StandardResponse,
@@ -15,7 +16,7 @@ import { EffectDTO } from '../effects.interface';
 import { addCardEffect } from './contants';
 
 export interface AddCardArgs {
-    destination: 'hand' | 'discard';
+    destination: keyof Expedition['currentNode']['data']['player']['cards'];
     cardId: Card['cardId'];
 }
 
@@ -27,7 +28,6 @@ export class AddCardEffect {
     constructor(
         private readonly playerService: PlayerService,
         private readonly cardService: CardService,
-        private readonly expeditionService: ExpeditionService,
     ) {}
 
     async handle(payload: EffectDTO<AddCardArgs>): Promise<void> {
@@ -54,27 +54,34 @@ export class AddCardEffect {
         const cardsToAdd: IExpeditionPlayerStateDeckCard[] = Array(currentValue)
             .fill(card)
             .map((card) => ({
+                ...card,
                 id: randomUUID(),
-                cardId: card.cardId,
-                name: card.name,
-                cardType: card.cardType,
-                energy: card.energy,
-                description: card.description,
-                isTemporary: false,
-                rarity: card.rarity,
-                properties: card.properties,
-                keywords: card.keywords,
-                showPointer: card.showPointer,
-                pool: card.pool,
-                isUpgraded: card.isUpgraded,
+                // cardId: card.cardId,
+                // name: card.name,
+                // cardType: card.cardType,
+                // energy: card.energy,
+                // description: card.description,
+                // isTemporary: false,
+                // rarity: card.rarity,
+                // properties: card.properties,
+                // keywords: card.keywords,
+                // showPointer: card.showPointer,
+                // pool: card.pool,
+                // isUpgraded: card.isUpgraded,
                 isActive: true,
             }));
 
-        // Now we save the cards on the destination deck
-        await this.expeditionService.updateHandPiles({
-            clientId: client.id,
-            [destination]: [...cards[destination], ...cardsToAdd],
-        });
+        cards[destination].push(...cardsToAdd);
+
+        // If destination is draw, we need to shuffle the cards
+        if (destination === 'draw') {
+            // Now we shuffle the cards
+            cards[destination] = shuffle(cards[destination]);
+        }
+
+        // Now we need to update the expedition state
+        ctx.expedition.markModified('currentNode.data.player.cards');
+        await ctx.expedition.save();
 
         client.emit(
             'PutData',
