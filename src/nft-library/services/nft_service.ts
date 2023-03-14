1a8cd8585474d0b1b3210a3fa101717eaf44e6ed
tonygrahn <118852733+tonygrahn@users.noreply.github.com>
2023-03-14 13:56:54 -0700

    prevent fatal json parsing in nft service code. remove temp testing code

--- src/nft-library/services/nft_service.ts
+++ src/nft-library/services/nft_service.ts
@@ -1,51 +1,64 @@
-import Moralis from "moralis"
+import Moralis from 'moralis';
 
 class NFTService {
-  async listByContracts (chain: number, address: string, contracts: string[]): Promise<any> {
-    const response = await Moralis.EvmApi.nft.getWalletNFTs({
-      address,
-      chain,
-    })
+    async listByContracts(
+        chain: number,
+        address: string,
+        contracts: string[],
+    ): Promise<any> {
+        const response = await Moralis.EvmApi.nft.getWalletNFTs({
+            address,
+            chain,
+        });
 
-    const tokens = response.raw.result
-    const nfts = []
+        const tokens = response.raw.result;
+        const nfts = [];
 
-    if(tokens != undefined) {
-      for (let i = 0; i < contracts.length; i++) {
-        const contract = contracts[i].toLowerCase()
-        const collection = []
+        if (tokens != undefined) {
+            for (let i = 0; i < contracts.length; i++) {
+                const contract = contracts[i].toLowerCase();
+                const collection = [];
 
-        for (let j = 0; j < tokens.length; j++) {
-          const token = tokens[j];
-          if(token.token_address.toLowerCase() == contract) {
-            collection.push({
-              token_id: token.token_id,
-              amount: token.amount,
-              owner_of: token.owner_of,
-              contract_type: token.contract_type,
-              name: token.name,
-              symbol: token.symbol,
-              token_uri: token.token_uri,
-              last_token_uri_sync: token.last_token_uri_sync,
-              last_metadata_sync: token.last_metadata_sync,
-              metadata: JSON.parse(token.metadata ?? ''),
-            })
-          }
+                for (let j = 0; j < tokens.length; j++) {
+                    const token = tokens[j];
+                    if (token.token_address === undefined) {
+                        console.log('token.token_address===undefined');
+                    }
+                    let metadata = '';
+                    try {
+                        metadata = JSON.parse(token.metadata ?? '');
+                    } catch (e) {
+                        metadata = '';
+                    }
+                    if (token.token_address.toLowerCase() == contract) {
+                        collection.push({
+                            token_id: token.token_id,
+                            amount: token.amount,
+                            owner_of: token.owner_of,
+                            contract_type: token.contract_type,
+                            name: token.name,
+                            symbol: token.symbol,
+                            token_uri: token.token_uri,
+                            last_token_uri_sync: token.last_token_uri_sync,
+                            last_metadata_sync: token.last_metadata_sync,
+                            metadata: metadata,
+                        });
+                    }
+                }
+
+                nfts.push({
+                    contract_address: contract,
+                    token_count: collection.length,
+                    tokens: collection,
+                });
+            }
         }
 
-        nfts.push({
-          contract_address: contract,
-          token_count: collection.length,
-          tokens: collection
-        })
-      }
+        return {
+            wallet: address,
+            tokens: nfts,
+        };
     }
-
-    return {
-      wallet: address,
-      tokens: nfts
-    }
-  }
 }
 
-export default new NFTService()
+export default new NFTService();
