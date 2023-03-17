cb0c72c811327ac3fbbc32f4897f3285260fc7c0
Ryan Abrams <rabrams@robotseamonstergames.com>
2023-03-16 20:30:46 -0500

    add queue to potion gateway

--- src/game/components/potion/potion.gateway.ts
+++ src/game/components/potion/potion.gateway.ts
@@ -4,6 +4,8 @@ import { FullSyncAction } from 'src/game/action/fullSync.action';
 import { corsSocketSettings } from 'src/socket/socket.enum';
 import { ExpeditionService } from '../expedition/expedition.service';
 import { PotionService } from './potion.service';
+import { ActionQueueService } from 'src/actionQueue/actionQueue.service';
+import { Logger } from '@nestjs/common';
 
 interface UsePotionDTO {
     potionId: string;
@@ -12,27 +14,44 @@ interface UsePotionDTO {
 
 @WebSocketGateway(corsSocketSettings)
 export class PotionGateway {
+    private readonly logger: Logger = new Logger(PotionGateway.name);
+
     constructor(
         private readonly expeditionService: ExpeditionService,
         private readonly potionService: PotionService,
         private readonly fullSyncAction: FullSyncAction,
+        private readonly actionQueueService: ActionQueueService,
     ) {}
 
     @SubscribeMessage('UsePotion')
     async use(client: Socket, payload: string): Promise<void> {
-        const { potionId, targetId } = JSON.parse(payload) as UsePotionDTO;
+        await this.actionQueueService.push(await this.expeditionService.getExpeditionIdFromClient(client),
+        async () => {
+            this.logger.debug('<USE POTION>');
 
-        const ctx = await this.expeditionService.getGameContext(client);
+            const { potionId, targetId } = JSON.parse(payload) as UsePotionDTO;
 
-        await this.potionService.use(ctx, potionId, targetId);
+            const ctx = await this.expeditionService.getGameContext(client);
 
-        await this.fullSyncAction.handle(client, false);
+            await this.potionService.use(ctx, potionId, targetId);
+
+            await this.fullSyncAction.handle(client, false);
+
+            this.logger.debug('</USE POTION>');
+        });
     }
 
     @SubscribeMessage('RemovePotion')
     async remove(client: Socket, potionId: string): Promise<void> {
-        const ctx = await this.expeditionService.getGameContext(client);
-        await this.potionService.remove(ctx, potionId);
-        await this.fullSyncAction.handle(client, false);
+        await this.actionQueueService.push(await this.expeditionService.getExpeditionIdFromClient(client),
+        async () => {        
+            this.logger.debug('<REMOVE POTION>');
+
+            const ctx = await this.expeditionService.getGameContext(client);
+            await this.potionService.remove(ctx, potionId);
+            await this.fullSyncAction.handle(client, false);
+            
+            this.logger.debug('</REMOVE POTION>');
+        });
     }
 }
