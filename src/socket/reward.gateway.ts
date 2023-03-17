045dec16cdc270878aa7b1e8a861422057eeff4f
Ryan Abrams <rabrams@robotseamonstergames.com>
2023-03-16 20:31:00 -0500

    FIX: add commented queue on reward gateway.

--- src/socket/reward.gateway.ts
+++ src/socket/reward.gateway.ts
@@ -4,13 +4,18 @@ import { FullSyncAction } from 'src/game/action/fullSync.action';
 import { ExpeditionService } from 'src/game/components/expedition/expedition.service';
 import { RewardService } from 'src/game/reward/reward.service';
 import { corsSocketSettings } from './socket.enum';
+import { ActionQueueService } from 'src/actionQueue/actionQueue.service';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway(corsSocketSettings)
 export class RewardGateway {
+    private readonly logger: Logger = new Logger(RewardGateway.name);
+
     constructor(
         private readonly expeditionService: ExpeditionService,
         private readonly fullSyncAction: FullSyncAction,
         private readonly rewardService: RewardService,
+        private readonly actionQueueService: ActionQueueService,
     ) {}
 
     @SubscribeMessage('RewardSelected')
@@ -18,13 +23,20 @@ export class RewardGateway {
         client: Socket,
         rewardId: string,
     ): Promise<string> {
-        // Get the game context
-        const ctx = await this.expeditionService.getGameContext(client);
+        // await this.actionQueueService.push(await this.expeditionService.getExpeditionIdFromClient(client),
+        // async () => {
+            //this.logger.debug('<REWARD SELECTED>');
+            
+            // Get the game context
+            const ctx = await this.expeditionService.getGameContext(client);
 
-        const response = await this.rewardService.takeReward(ctx, rewardId);
+            const response = await this.rewardService.takeReward(ctx, rewardId);
 
-        await this.fullSyncAction.handle(client, false);
+            await this.fullSyncAction.handle(client, false);
 
-        return response;
+            return response; // THIS PROBABLY BREAKS IN A CHAIN OF TYPE Promise<Void>. Rework to emit a response instead of returning.
+            
+            //this.logger.debug('<REWARD SELECTED>');
+        // });
     }
 }
