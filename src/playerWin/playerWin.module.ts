b03117ff4539171503181d6e4321d75ad2100ecc
Juan David Melo Colmenares <ontimond@gmail.com>
2023-03-22 16:55:44 -0500

    Add CharacterModule to PlayerWinModule and WalletModule

--- src/playerWin/playerWin.module.ts
+++ src/playerWin/playerWin.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { KindagooseModule } from 'kindagoose';
+import { CharacterModule } from 'src/game/components/character/character.module';
 import { PlayerWin } from './playerWin.schema';
 import { PlayerWinService } from './playerWin.service';
 
 @Module({
-    imports: [KindagooseModule.forFeature([PlayerWin])],
+    imports: [KindagooseModule.forFeature([PlayerWin]), CharacterModule],
     providers: [PlayerWinService],
     exports: [PlayerWinService],
 })
