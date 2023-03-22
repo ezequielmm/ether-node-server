69ebac5b531c45da03974c34f25b985853fef175
tonygrahn <118852733+tonygrahn@users.noreply.github.com>
2023-03-22 15:34:38 -0700

    use only current contest. add limit and duration of expedition

--- src/api/highscore.controller.ts
+++ src/api/highscore.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { ExpeditionService } from "../game/components/expedition/expedition.service";
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ExpeditionService } from '../game/components/expedition/expedition.service';
+import { ContestService } from '../game/contest/contest.service';
 
 @Controller('highscrore')
 export class HighscroreController {
     constructor(
         private readonly expeditionService: ExpeditionService,
-    ){}
+        private contestService: ContestService,
+    ) {}
     @Get('/')
-    async getHighscrores(): Promise<any> {
-        const what = { value: 'nothing' };
-        const who = await this.expeditionService.findTopScores();
+    async getHighscrores(@Query('limit') limit: number): Promise<any> {
+        if (!limit) limit = 10;
+        const contest = await this.contestService.findActive();
+        const who = await this.expeditionService.findTopScores(
+            contest.event_id,
+            limit,
+        );
         return who;
     }
 }
--- src/game/components/expedition/expedition.service.ts
+++ src/game/components/expedition/expedition.service.ts
@@ -391,23 +391,31 @@ export class ExpeditionService {
         await ctx.expedition.save();
     }
 
-    async findTopScores(): Promise<any[]> {
+    async findTopScores(event_id: string, limit: number): Promise<any[]> {
+        limit = limit > 50 ? 50 : limit;
+        limit = limit < 10 ? 10 : limit;
         const expedition = await this.expedition
-            .find()
+            .find({ 'contest.event_id': event_id })
             .sort({ 'finalScore.totalScore': -1 })
-            .limit(50);
+            .limit(limit);
 
         const finalScores = [];
         expedition.forEach((item) => {
             if (item.finalScore) {
+                const endedAt = item.endedAt;
+                const startedAt = item.createdAt;
+                const duration = Math.floor(
+                    (endedAt.getTime() - startedAt.getTime()) / 1000,
+                );
                 const summary = {
                     totalScore: item.finalScore.totalScore,
                     playerName: item.playerState.playerName,
                     characterClass: item.playerState.characterClass,
-                    endedAt: item.endedAt,
-                }
+                    duration,
+                    endedAt,
+                };
                 finalScores.push(summary);
-            };
+            }
         });
 
         return finalScores;
