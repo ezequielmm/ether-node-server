508964b837b32997913840f40e7cc344f217ce82
Javier Emmanuel Mercedes <manuelmercedez10@gmail.com>
2023-03-22 11:30:29 -0400

    linted contest service and added proper return types

--- src/game/contest/contest.schema.ts
+++ src/game/contest/contest.schema.ts
@@ -1,17 +1,11 @@
 import { ModelOptions, Prop } from '@typegoose/typegoose';
-import { Schema } from 'mongoose';
 
 @ModelOptions({
     schemaOptions: { collection: 'contests', versionKey: false },
 })
 export class Contest {
-    _id: Schema.Types.ObjectId;
-
     @Prop()
     map_id: string;
-
-    @Prop()
-    event_id: string;
 
     @Prop()
     available_at: Date;
