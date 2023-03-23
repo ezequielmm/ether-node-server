c4bd097da1333228ca694164783367c012015330
Juan David Melo Colmenares <ontimond@gmail.com>
2023-03-23 18:13:37 -0500

    Refactor Highscore controller to remove Param dependency

--- src/api/highscore.controller.ts
+++ src/api/highscore.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ExpeditionService } from '../game/components/expedition/expedition.service';
 import { ContestService } from '../game/contest/contest.service';
 
