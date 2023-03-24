247a6c9feca869b3c0256877ba09c8ee89876659
Ryan Abrams <rabrams@robotseamonstergames.com>
2023-03-24 11:26:04 -0500

    enqueue encounter, and wrap response as emit

--- src/socket/encounter.gateway.ts
+++ src/socket/encounter.gateway.ts
@@ -4,6 +4,7 @@ import { Logger } from '@nestjs/common';
 import { Socket } from 'socket.io';
 import { EncounterService } from '../game/components/encounter/encounter.service';
 import { ExpeditionService } from 'src/game/components/expedition/expedition.service';
+import { ActionQueueService } from 'src/actionQueue/actionQueue.service';
 
 @WebSocketGateway(corsSocketSettings)
 export class EncounterGateway {
@@ -12,23 +13,35 @@ export class EncounterGateway {
     constructor(
         private readonly encounterService: EncounterService,
         private readonly expeditionService: ExpeditionService,
+        private readonly actionQueueService: ActionQueueService,
     ) {}
 
     @SubscribeMessage('EncounterChoice')
     async handleEncounterChoice(
         client: Socket,
         choiceIdx: string,
-    ): Promise<string> {
-        const ctx = await this.expeditionService.getGameContext(client);
+    ): Promise<void> {
+        await this.actionQueueService.push(
+            await this.expeditionService.getExpeditionIdFromClient(client.id),
+            async () => {
+                this.logger.debug('<ENCOUNTER CHOICE>');
+                const ctx = await this.expeditionService.getGameContext(client);
 
-        this.logger.log(
-            ctx.info,
-            `Client ${client.id} trigger message "EncounterChoice" with choiceId: ${choiceIdx}`,
-        );
+                this.logger.log(
+                    ctx.info,
+                    `Client ${client.id} trigger message "EncounterChoice" with choiceId: ${choiceIdx}`,
+                );
 
-        return await this.encounterService.encounterChoice(
-            client,
-            parseInt(choiceIdx),
+                ctx.client.emit(
+                    'EncounterChoiceResponse',
+                    await this.encounterService.encounterChoice(
+                        client,
+                        parseInt(choiceIdx),
+                    ),
+                );
+
+                this.logger.debug('</ENCOUNTER CHOICE>');
+            }
         );
     }
 }
