501dbaee8f64d0067471fe3f3fdcdb9be78ce3f4
Ryan Abrams <rabrams@robotseamonstergames.com>
2023-04-14 21:54:16 -0500

    remove strategy

--- src/game/map/map.module.ts
+++ src/game/map/map.module.ts
@@ -2,7 +2,6 @@ import { forwardRef, Module } from '@nestjs/common';
 import { CombatModule } from 'src/game/combat/combat.module';
 import { MapService } from './map.service';
 import { CampNodeStrategy } from './strategies/camp-node-strategy';
-import { EncounterNodeStrategy } from './strategies/encounter-node-strategy';
 import { PortalNodeStrategy } from './strategies/portal-node-strategy';
 import { RoyalHouseNodeStrategy } from './strategies/royal-house-node-strategy';
 import { TreasureModule } from 'src/game/treasure/treasure.module';
@@ -20,7 +19,6 @@ import { MerchantModule } from 'src/game/merchant/merchant.module';
         MapService,
         PortalNodeStrategy,
         RoyalHouseNodeStrategy,
-        EncounterNodeStrategy,
         CampNodeStrategy,
     ],
     exports: [MapService],
