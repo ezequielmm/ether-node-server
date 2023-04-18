a614625ed46722d62564c0cb057cc288611a4cc9
Ryan Abrams <rabrams@robotseamonstergames.com>
2023-04-18 10:01:55 -0500

    Set character drop rate defaults to client spec (#212)
    * Set character drop rate defaults to client spec
    
    * make uncommon more rare on villagers

--- src/game/components/character/data/knight.character.ts
+++ src/game/components/character/data/knight.character.ts
@@ -39,10 +39,10 @@ export const KnightCharacter: Character = {
         },
     ],
     lootboxRarity: {
-        common: 20,
-        uncommon: 20,
+        common: 45,
+        uncommon: 25,
         rare: 20,
-        epic: 20,
-        legendary: 20,
+        epic: 9.8,
+        legendary: 0.2,
     },
 };
--- src/game/components/character/data/villager.character.ts
+++ src/game/components/character/data/villager.character.ts
@@ -39,8 +39,8 @@ export const VillagerCharacter: Character = {
         },
     ],
     lootboxRarity: {
-        common: 60,
-        uncommon: 40,
+        common: 80,
+        uncommon: 20,
         rare: 0,
         epic: 0,
         legendary: 0,
