0907f61ff42fbfbc31797d4e33ac662c93267ecc
Camilo <corquera@kreitech.com.uy>
2023-06-27 19:42:44 +0200

    leaderboard and card description fixed

--- src/game/action/getRewards.action.ts
+++ src/game/action/getRewards.action.ts
@@ -2,10 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { NodeType } from '../components/expedition/node-type';
 import { Reward } from '../components/expedition/expedition.interface';
 import { ExpeditionService } from '../components/expedition/expedition.service';
+import { CardDescriptionFormatter } from 'src/game/cardDescriptionFormatter/cardDescriptionFormatter';
+import { IExpeditionNodeReward } from '../components/expedition/expedition.enum';
+import { Card } from '../components/card/card.schema';
+import { CardService } from '../components/card/card.service';
 
 @Injectable()
 export class GetRewardsAction {
-    constructor(private readonly expeditionService: ExpeditionService) {}
+    constructor(private readonly expeditionService: ExpeditionService,
+                private readonly cardService: CardService) {}
 
     async handle(clientId: string): Promise<{ rewards: Reward[] }> {
         const currentNode = await this.expeditionService.getCurrentNode({
@@ -17,7 +22,7 @@ export class GetRewardsAction {
         switch (currentNode.nodeType) {
             case NodeType.Combat:
                 rewards = currentNode.showRewards
-                    ? currentNode.data.rewards
+                    ? await this.manageCardDescriptions(currentNode.data.rewards)
                     : [];
                 break;
             case NodeType.Treasure:
@@ -29,5 +34,16 @@ export class GetRewardsAction {
         }
 
         return { rewards };
+    }
+
+    private async manageCardDescriptions(rewards:Reward[]):Promise<Reward[]>{
+        for(let reward of rewards){
+            if (reward.type === IExpeditionNodeReward.Card){
+                let card:Card = await this.cardService.findById(reward.card.cardId);
+                reward.card.description = CardDescriptionFormatter.process(card);
+            }
+        }
+
+        return rewards;
     }
 }
