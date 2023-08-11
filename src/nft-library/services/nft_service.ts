a74ac9bf3f55618a70ee300d15744398ef04b3af
Camilo <corquera@kreitech.com.uy>
2023-08-11 10:27:35 +0200

    Small refactor on nft service

--- src/nft-library/services/nft_service.ts
+++ src/nft-library/services/nft_service.ts
@@ -7,18 +7,6 @@ import { OwnedNft } from 'alchemy-sdk';
 @Injectable()
 export class NFTService {
 
-    // Hotfix Demo:
-    
-    //- Arbitrum:
-    private MAIN_VILLAGER_CONTRACT_ID = "0x292Ff0F0c19373dd9c50faBba574Aaaf6E1BC11B"
-    private TEST_VILLAGER_CONTRACT_ID = "0x913dB69145f33Af291F46E980e4c0CaBBfcC27AA"
-    private MAIN_BLESSED_CONTRACT_ID  = "0x73DdCE2656c343dc6655e76202768c703D1f540B"
-    private TEST_BLESSED_CONTRACT_ID  = "0xbFfd759b9F7d07ac76797cc13974031Eb23e5757"
-    private MAIN_KNIGHT_CONTRACT_ID   = "0xb52d71C3DdE0cEE0faD2dCE0a9cA33fDfE06aEc9"
-    private TEST_KNIGHT_CONTRACT_ID   = "0x450210F1f501E94DB0DeA2eD1Cfc880aa803931a"
-    
-    // Hotfix Demo
-
     constructor(private readonly alchemyService: AlchemyService, 
                 private readonly configService: ConfigService){}
 
@@ -65,24 +53,15 @@ export class NFTService {
             };
         });
 
-        // aggregate results across multiple pages
-        const net = this.configService.get("NFT_SERVICE_NET");
-        const alchemySettings = this.alchemyService.getInstance();
 
+        const alchemySettings = this.alchemyService.getInstance();
         const pageSize = amount <= 100 ? amount : 100;
 
-        let villagerAddress = net === AlchemyService.MAINNET ? this.MAIN_VILLAGER_CONTRACT_ID : this.TEST_VILLAGER_CONTRACT_ID;
-        const villagers = await alchemySettings.nft.getNftsForOwner(walletAddress, { pageSize, contractAddresses: [villagerAddress] });
-
-        let blessedVillagerAddress = net === AlchemyService.MAINNET ? this.MAIN_BLESSED_CONTRACT_ID : this.TEST_BLESSED_CONTRACT_ID;
-        const blessedVillagers = await alchemySettings.nft.getNftsForOwner(walletAddress, { pageSize, contractAddresses: [blessedVillagerAddress] });
-
-        let knightAddress = net === AlchemyService.MAINNET ? this.MAIN_KNIGHT_CONTRACT_ID : this.TEST_KNIGHT_CONTRACT_ID;
-        const knights = await alchemySettings.nft.getNftsForOwner(walletAddress, { pageSize, contractAddresses: [knightAddress] });
-
-        this.loadNftsByCharacterType(tokenCollections, villagerAddress.toLowerCase(), villagers.ownedNfts,  amount);
-        this.loadNftsByCharacterType(tokenCollections, blessedVillagerAddress.toLowerCase(), blessedVillagers.ownedNfts, amount);
-        this.loadNftsByCharacterType(tokenCollections, knightAddress.toLowerCase(), knights.ownedNfts, amount);
+        for(let address of tokenAddresses){
+            console.log(address)
+            const nftsByCharacter = await alchemySettings.nft.getNftsForOwner(walletAddress, { pageSize, contractAddresses: [address] });
+            this.loadNftsByCharacterType(tokenCollections, address.toLowerCase(), nftsByCharacter.ownedNfts,  amount);
+        }
 
         // return collections in an order matching the provided tokenAddresses
         const collections = [];
