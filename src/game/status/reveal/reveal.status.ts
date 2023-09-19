869bf553fc76ad6dfb22e8fbe5e5a2e33d29e7d1
Camilo <corquera@kreitech.com.uy>
2023-09-19 18:23:19 +0200

    reveal

--- src/game/status/reveal/reveal.status.ts
+++ src/game/status/reveal/reveal.status.ts
@@ -2,9 +2,6 @@ import { Injectable } from "@nestjs/common";
 import { StatusEventDTO, StatusEventHandler } from "../interfaces";
 import { StatusDecorator } from "../status.decorator";
 import { revealStatus } from "./constants";
-import { OnEvent } from "@nestjs/event-emitter";
-import { GameContext } from "src/game/components/interfaces";
-import { EVENT_BEFORE_ENEMIES_TURN_START } from "src/game/constants";
 import { StatusService } from "../status.service";
 import { EnemyService } from "src/game/components/enemy/enemy.service";
 
@@ -25,25 +22,16 @@ export class RevealStatus implements StatusEventHandler {
 
         // Remove status if counter is 0
         if (status.args.counter === 0) {
-            console.log("Acá mataríamos al cofre e invocaríamos al mimic")
+            console.log("Source")
+            console.log(source)
+            console.log("-----------------------------------------------")
+            console.log("target:")
+            console.log(target)
             remove();
+            this.enemyService.setHp(ctx, source.value.id, 0);
+
         } else {
             update(status.args);
         }
     }
-
-    // @OnEvent(EVENT_BEFORE_ENEMIES_TURN_START)
-    // async onEnemiesTurnStart(args: { ctx: GameContext }): Promise<void> {
-    //     const { ctx } = args;
-    //     const enemies = this.enemyService.getAll(ctx);
-
-    //     for (const enemy of enemies) {
-    //         await this.statusService.decreaseCounterAndRemove(
-    //             ctx,
-    //             enemy.value.statuses,
-    //             enemy,
-    //             revealStatus,
-    //         );
-    //     }
-    // }
 }
