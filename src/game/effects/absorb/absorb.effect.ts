9271c82259cc7695b7f7e4c157e55b383012f5ba
Camilo <corquera@kreitech.com.uy>
2023-09-21 12:49:39 +0200

    test absorb effect

--- src/game/effects/absorb/absorb.effect.ts
+++ src/game/effects/absorb/absorb.effect.ts
@@ -15,13 +15,19 @@ export class AbsorbEffect implements EffectHandler {
     async handle(dto: EffectDTO<Record<string, any>>): Promise<void> {
         
         const { ctx, source } = dto;
+        console.log("----------------------------------------------------")
+        console.log("Inside AbsorbEffect");
         
         if(EnemyService.isEnemy(source)){
+            console.log("Source is an Enemy");
+            console.log("Args:");
+            console.log(dto.args)
             const healAmount = dto.args.currentValue;
             if(healAmount && healAmount > 0){
                 const newHp = source.value.hpCurrent;
                 this.enemyService.setHp(ctx, source.value.enemyId, newHp);
             }
         }
+        console.log("----------------------------------------------------")
     }
 }
