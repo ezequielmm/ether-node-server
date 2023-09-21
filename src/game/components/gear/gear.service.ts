9455628733bde6ec4ecd3254b4d47e301c6979c6
ezequielmm <ezequiel@manufactory.gg>
2023-09-20 23:51:38 -0300

    experiment with gear

--- src/game/components/gear/gear.service.ts
+++ src/game/components/gear/gear.service.ts
@@ -52,17 +52,49 @@ export class GearService {
     async getLootbox(
         size: number,
         rarities?: ILootboxRarityOdds,
-    ): Promise<Gear[]> {
+        userGear: Gear[] = [],
+      ): Promise<Gear[]> {
+        //console.log('Starting to generate lootbox...');
         const gear_list: Gear[] = [];
+        const uniqueGearIds: Set<string> = new Set();
+    
+        userGear.forEach((gear) => uniqueGearIds.add(gear.gearId.toString()));
+    
+       /* console.log(
+          `Initial unique gear IDs: ${Array.from(uniqueGearIds).join(', ')}`,
+        );
+    */
 
-        for (let i = 0; i < size; i++) {
-            const one_gear = await this.getOneGear(
-                this.selectRandomRarity(rarities),
-            );
-            gear_list.push(one_gear);
-        }
-
+        // Gear que va a salir en la lootbox
+        let targetGearSet = '';
+        let allGear: Gear[] = await this.getAllGear();
+        allGear = allGear.filter((gear) => gear.name === targetGearSet);
+    
+        let itemAdded = false;
+        let targetRarity = this.selectRandomRarity(rarities);
+    
+        while (itemAdded === false) {
+          const newGear = this.getRandomGearByRarity(allGear, targetRarity);
+    
+          if (uniqueGearIds.has(newGear.gearId.toString())) {
+           /* console.log(
+              `Repeated: ${newGear.gearId.toString()} - ${newGear.rarity}`,
+            );*/
+            targetRarity = this.downgradeRarity(targetRarity);
+    
+            if (targetRarity === null) {
+              //console.log('Target rarity null, break');
+              break;
+            }
+          } else {
+            console.log(`Adding: ${newGear.gearId} - ${newGear.rarity}`);
+            gear_list.push(newGear);
+            uniqueGearIds.add(newGear.gearId.toString());
+            itemAdded = true;
+          }
+    
         return gear_list;
+      }
     }
 
     async getOneGear(rarity: GearRarityEnum): Promise<Gear> {
@@ -81,4 +113,49 @@ export class GearService {
 
         return gear;
     }
+
+    async getAllGear(): Promise<Gear[] | null> {
+        try {
+          const allGear = await this.gearModel.find({});
+          return allGear;
+        } catch (error) {
+          console.error('An error occurred while fetching all gear:', error);
+          return null;
+        }
+      }
+
+      private getRandomGearByRarity(
+        allGear: Gear[],
+        targetRarity: GearRarityEnum,
+      ): Gear | null {
+        // Filter the allGear array by the target rarity
+        const filteredGear = allGear.filter((gear) => gear.rarity === targetRarity);
+    
+        // Use lodash's sample method to get a random gear item
+        return sample(filteredGear) || null;
+      }
+
+      private downgradeRarity(
+        currentRarity: GearRarityEnum,
+      ): GearRarityEnum | null {
+        //console.log(`Current rarity: ${currentRarity}. Attempting to downgrade...`); // Added log
+    
+        switch (currentRarity) {
+          case GearRarityEnum.Legendary:
+            //console.log(`Downgraded from Legendary to Epic`); // Added log
+            return GearRarityEnum.Epic;
+          case GearRarityEnum.Epic:
+            //console.log(`Downgraded from Epic to Rare`); // Added log
+            return GearRarityEnum.Rare;
+          case GearRarityEnum.Rare:
+            //console.log(`Downgraded from Rare to Uncommon`); // Added log
+            return GearRarityEnum.Uncommon;
+          case GearRarityEnum.Uncommon:
+            //console.log(`Downgraded from Uncommon to Common`); // Added log
+            return GearRarityEnum.Common;
+          default:
+            //console.log('No lower rarity available'); // Added log
+            return null;
+        }
+      }
 }
