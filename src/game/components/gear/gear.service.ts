494b41be3dba2e778b803cbb6b5902b269ec1f7f
ezequielmm <ezequiel@manufactory.gg>
2023-09-21 08:41:59 -0300

    new disable old methods

--- src/game/components/gear/gear.service.ts
+++ src/game/components/gear/gear.service.ts
@@ -67,16 +67,47 @@ export class GearService {
     rarities?: ILootboxRarityOdds,
     userGear: Gear[] = [],
   ): Promise<Gear[]> {
+    //console.log('Starting to generate lootbox...');
     const gear_list: Gear[] = [];
+    const uniqueGearIds: Set<string> = new Set();
 
-    for (let i = 0; i < size; i++) {
-        const one_gear = await this.getOneGear(
-            this.selectRandomRarity(rarities),
-        );
-        gear_list.push(one_gear);
-    }
+    userGear.forEach((gear) => uniqueGearIds.add(gear.gearId.toString()));
+
+   /* console.log(
+      `Initial unique gear IDs: ${Array.from(uniqueGearIds).join(', ')}`,
+    );
+*/
+    let targetGearSet = 'Siege';
+    let allGear: Gear[] = await this.getAllGear();
+    allGear = allGear.filter((gear) => gear.name === targetGearSet);
+
+    let itemAdded = false;
+    let targetRarity = this.selectRandomRarity(rarities);
+
+    while (itemAdded === false) {
+      const newGear = this.getRandomGearByRarity(allGear, targetRarity);
+
+      if (uniqueGearIds.has(newGear.gearId.toString())) {
+       /* console.log(
+          `Repeated: ${newGear.gearId.toString()} - ${newGear.rarity}`,
+        );*/
+        targetRarity = this.downgradeRarity(targetRarity);
+
+        if (targetRarity === null) {
+          //console.log('Target rarity null, break');
+          break;
+        }
+      } else {
+        console.log(`Adding: ${newGear.gearId} - ${newGear.rarity}`);
+        gear_list.push(newGear);
+        uniqueGearIds.add(newGear.gearId.toString());
+        itemAdded = true;
+      }
+
+    
 
     return gear_list;
+  }
 }
 
   private downgradeRarity(
@@ -113,6 +144,7 @@ export class GearService {
     // Use lodash's sample method to get a random gear item
     return sample(filteredGear) || null;
   }
+
   async getOneGear(rarity: GearRarityEnum): Promise<Gear> {
     const availableGear = await this.gearModel.find({ rarity });
     return sample(availableGear);
