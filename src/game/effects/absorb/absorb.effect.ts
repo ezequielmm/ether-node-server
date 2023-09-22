7e900ca02f581b6acf6ce8f9420567924f120c8f
Camilo <corquera@kreitech.com.uy>
2023-09-22 13:50:40 +0200

    testing hatching and absorb

--- src/game/effects/absorb/absorb.effect.ts
+++ src/game/effects/absorb/absorb.effect.ts
@@ -3,6 +3,9 @@ import { EffectDecorator } from "../effects.decorator";
 import { EffectHandler, EffectDTO } from "../effects.interface";
 import { absorbEffect } from "./constants";
 import { EnemyService } from "src/game/components/enemy/enemy.service";
+import { CombatQueueTargetEffectTypeEnum } from "src/game/components/combatQueue/combatQueue.enum";
+import { GameContext, ExpeditionEntity } from "src/game/components/interfaces";
+import { CombatQueueService } from "src/game/components/combatQueue/combatQueue.service";
 
 @EffectDecorator({
     effect: absorbEffect,
@@ -10,11 +13,11 @@ import { EnemyService } from "src/game/components/enemy/enemy.service";
 @Injectable()
 export class AbsorbEffect implements EffectHandler {
 
-    constructor(private readonly enemyService:EnemyService){}
+    constructor(private readonly enemyService:EnemyService, private readonly combatQueueService:CombatQueueService){}
 
     async handle(dto: EffectDTO<Record<string, any>>): Promise<void> {
         
-        const { ctx, source } = dto;
+        const { ctx, source, target } = dto;
         console.log("----------------------------------------------------")
         console.log("Inside AbsorbEffect");
         
@@ -24,10 +27,41 @@ export class AbsorbEffect implements EffectHandler {
             console.log(dto.args)
             const healAmount = dto.args.currentValue;
             if(healAmount && healAmount > 0){
-                const newHp = source.value.hpCurrent + healAmount;
-                this.enemyService.setHp(ctx, source.value.enemyId, newHp);
+                const hpCurrent = source.value.hpCurrent;
+                let newHp = hpCurrent + healAmount;
+                newHp = await this.enemyService.setHp(ctx, source.value.enemyId, newHp);
+
+                await this.sendToCombatQueue(
+                    ctx,
+                    source,
+                    target,
+                    newHp - hpCurrent,
+                    newHp,
+                );
             }
         }
         console.log("----------------------------------------------------")
+    }
+
+    private async sendToCombatQueue(
+        ctx: GameContext,
+        source: ExpeditionEntity,
+        target: ExpeditionEntity,
+        healthDelta: number,
+        finalHealth: number,
+    ): Promise<void> {
+        await this.combatQueueService.push({
+            ctx,
+            source,
+            target,
+            args: {
+                effectType: CombatQueueTargetEffectTypeEnum.Heal,
+                defenseDelta: 0,
+                finalDefense: 0,
+                healthDelta,
+                finalHealth,
+                statuses: [],
+            },
+        });
     }
 }
