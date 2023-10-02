9b485d4a617acce47d32987c610f55a53ca55a5e
Camilo <corquera@kreitech.com.uy>
2023-10-02 10:16:26 +0200

    mold polyp tests

--- src/game/effects/immolate/immolate.effect.ts
+++ src/game/effects/immolate/immolate.effect.ts
@@ -19,6 +19,11 @@ export class ImmolateEffect implements EffectHandler {
 
         if(EnemyService.isEnemy(source)){
             const updatedEnemies = enemies.filter(enemy => enemy.id !== source.value.id);
+
+            console.log("Immolate Efefct --------------- ----------------------------------------------")
+            console.log(updatedEnemies)
+            console.log("------------------------------- ----------------------------------------------")
+
             ctx.expedition.currentNode.data.enemies = updatedEnemies;
             ctx.expedition.markModified('currentNode.data.enemies');
             await ctx.expedition.save();
