3cff02748352683201370a9032f43dba66425e36
Camilo <corquera@kreitech.com.uy>
2023-10-02 11:24:14 +0200

    immolate tests

--- src/game/effects/immolate/immolate.effect.ts
+++ src/game/effects/immolate/immolate.effect.ts
@@ -4,6 +4,8 @@ import { EffectDecorator } from "../effects.decorator";
 import { EffectHandler, EffectDTO } from "../effects.interface";
 import { immolateEffect } from "./constants";
 import { StandardResponse, SWARMessageType, SWARAction } from "src/game/standardResponse/standardResponse";
+import { EventEmitter2 } from "@nestjs/event-emitter";
+import { EVENT_AFTER_DAMAGE_EFFECT } from "src/game/constants";
 
 @EffectDecorator({
     effect: immolateEffect,
@@ -11,7 +13,7 @@ import { StandardResponse, SWARMessageType, SWARAction } from "src/game/standard
 @Injectable()
 export class ImmolateEffect implements EffectHandler {
     
-    constructor(private readonly enemyService: EnemyService) {}
+    constructor(private readonly eventEmitter: EventEmitter2) {}
 
     async handle(dto: EffectDTO): Promise<void> {
         const { source, ctx } = dto;
@@ -36,6 +38,11 @@ export class ImmolateEffect implements EffectHandler {
                     data: source.value,
                 }),
             );
+
+            await this.eventEmitter.emitAsync(EVENT_AFTER_DAMAGE_EFFECT, {
+                ctx,
+                damageDealt: source.value.hpCurrent,
+            });
         }
     }
 
