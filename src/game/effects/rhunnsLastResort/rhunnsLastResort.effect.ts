ba18dbdd8fb739396568820082b7d2f4e0f4243c
Camilo <corquera@kreitech.com.uy>
2023-10-16 14:09:50 +0200

    testng rhunn potion

--- src/game/effects/rhunnsLastResort/rhunnsLastResort.effect.ts
+++ src/game/effects/rhunnsLastResort/rhunnsLastResort.effect.ts
@@ -4,6 +4,7 @@ import { EffectDTO, EffectHandler } from "../effects.interface";
 import { rhunnsLastResortEffect } from "./constants";
 import { getRandomBetween } from "src/utils";
 import { EnemyService } from "src/game/components/enemy/enemy.service";
+import { PlayerService } from "src/game/components/player/player.service";
 
 export interface RhunnsLastResortArgs {
     minDamage: number;
@@ -16,21 +17,22 @@ export interface RhunnsLastResortArgs {
 @Injectable()
 export class RhunnsLastResortEffect implements EffectHandler {
     
-    constructor(private readonly enemyService:EnemyService){}
+    constructor(private readonly enemyService:EnemyService,
+                private readonly playerService:PlayerService){}
 
     async handle(dto: EffectDTO<RhunnsLastResortArgs>): Promise<void> {
         
         const target = dto.target;
         const ctx    = dto.ctx;
 
-        console.log("RhunnsLastResort Effect-------------------------");
-        console.log("RandomEnemy: ");
         if(EnemyService.isEnemy(target)){
-            console.log(target.value.enemyId);
             const damage = getRandomBetween(dto.args.minDamage, dto.args.maxDamage);
 
-            console.log("Random damage: " + damage);
-            console.log("----------------------------------------")
+            const playerNewHp = await this.playerService.breach(ctx, damage);
+
+            if(playerNewHp > 0){
+                console.log("Intercambiar vida con el enemy")
+            }
         }
 
     }
