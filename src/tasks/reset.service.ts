95e7c13d69b2dce0aa55518fb6ee544c06d9c0af
ezequielmm <ezequiel@manufactory.gg>
2023-10-17 22:46:49 -0300

    change reset inprogress to every week

--- src/tasks/reset.service.ts
+++ src/tasks/reset.service.ts
@@ -37,7 +37,7 @@ export class ResetService {
         private readonly oldExpedition: ReturnModelType<typeof OldExpedition>,    
         ) { }
 
-    @Cron(CronExpression.EVERY_DAY_AT_11PM, {
+    @Cron(CronExpression.EVERY_WEEKDAY, {
         name: 'Reset in progress expeditions',
         timeZone: 'UTC',
     })
