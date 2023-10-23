acda508c18e3bd66cd3b51adbf986deb03a1956b
ezequielmm <ezequiel@manufactory.gg>
2023-10-22 21:32:13 -0300

    si al menos una expedicion de la wallet completo el stage 1 da true toda la funcion

--- src/api/isfirstsage.controller.ts
+++ src/api/isfirstsage.controller.ts
@@ -19,26 +19,10 @@ import {
 } from '@nestjs/swagger';
 import { AuthGuard } from '../auth/auth.guard';
 import { ExpeditionService } from '../game/components/expedition/expedition.service';
-import { ExpeditionStatusEnum } from 'src/game/components/expedition/expedition.enum';
-import { InitExpeditionProcess } from 'src/game/process/initExpedition.process';
-import {
-    ScoreCalculatorService,
-    ScoreResponse,
-} from 'src/game/scoreCalculator/scoreCalculator.service';
-import { GearItem } from '../playerGear/gearItem';
-import { PlayerGearService } from '../playerGear/playerGear.service';
-import { ContestService } from '../game/contest/contest.service';
-import { Contest } from 'src/game/contest/contest.schema';
-import { Expedition, IPlayerToken } from 'src/game/components/expedition/expedition.schema';
-import { PlayerWinService } from 'src/playerWin/playerWin.service';
-import { AuthorizedRequest } from 'src/auth/auth.types';
-import { TroveService } from 'src/trove/trove.service';
 
 class IsFinishedFirstStageApiDTO {
     @ApiProperty()
-    readonly wallet: string;    
-    @ApiProperty()
-    readonly expeditionId: string;
+    readonly wallet: string;
 }
 
 @ApiBearerAuth()
@@ -48,25 +32,14 @@ class IsFinishedFirstStageApiDTO {
 export class IsFirstStageFinishedController {
     constructor(
         private readonly expeditionService: ExpeditionService,
-        private readonly initExpeditionProcess: InitExpeditionProcess,
-        private readonly scoreCalculatorService: ScoreCalculatorService,
-        private readonly playerGearService: PlayerGearService,
-        private readonly playerWinService: PlayerWinService,
-        private readonly troveService: TroveService,
-        private contestService: ContestService,
     ) { }
 
     private readonly logger: Logger = new Logger(IsFirstStageFinishedController.name);
 
     @ApiOperation({ summary: 'Get player owned gear list for API' })
-    @Get('/isFinishedFirstStage') async getList(@Query('expeditionId') expeditionId: string, @Query('wallet') wallet: string): Promise<boolean> {
-        // confirm token (security layer)
-        // let validToken = await this.checkSecurityToken({ wallet, token });
-        // if(!validToken) {
-        //     throw new UnauthorizedException('Bad Token');
-        // }
+    @Get('/isFinishedFirstStage') async getList(@Query('wallet') wallet: string): Promise<boolean> {
 
-        return await this.expeditionService.checkCurrentStage(expeditionId, wallet)
+        return await this.expeditionService.checkCurrentStage(wallet)
 
     }
 }
