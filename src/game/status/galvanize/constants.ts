30610c64ca6615c1cc7940f5415b76b1f4c2ae74
Martin Roux <mroux@kreitech.com.uy>
2023-10-28 14:37:37 -0300

    galvanize test 3

--- src/game/status/galvanize/constants.ts
+++ src/game/status/galvanize/constants.ts
@@ -1,17 +1,19 @@
+import { EVENT_AFTER_CARD_PLAY } from 'src/game/constants';
 import {
     StatusCounterType,
     StatusDirection,
     StatusEffect,
+    StatusEvent,
     StatusTrigger,
     StatusType,
 } from '../interfaces';
+import { attachStatusEffect } from 'src/game/effects/attachStatus/constants';
 import { damageEffect } from 'src/game/effects/damage/constants';
 
-export const galvanize: StatusEffect = {
+export const galvanize: StatusEvent = {
     name: 'galvanize',
     type: StatusType.Buff,
-    counterType: StatusCounterType.None,
-    trigger: StatusTrigger.Effect,
-    effects: [damageEffect],
-    direction: StatusDirection.Incoming,
+    counterType: StatusCounterType.Counter,
+    trigger: StatusTrigger.Event,
+    event: EVENT_AFTER_CARD_PLAY,
 };
