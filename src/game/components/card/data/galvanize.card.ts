2e0fcf927412e5541d282f730d523d6da887a951
Martin Roux <mroux@kreitech.com.uy>
2023-10-28 12:17:24 -0300

    galvanize first test

--- src/game/components/card/data/galvanize.card.ts
+++ src/game/components/card/data/galvanize.card.ts
@@ -1,13 +1,15 @@
 import { defenseEffect } from 'src/game/effects/defense/constants';
 import { CardRarityEnum, CardTypeEnum, CardTargetedEnum } from '../card.enum';
 import { Card } from '../card.schema';
+import { attachStatusEffect } from 'src/game/effects/attachStatus/constants';
+import { galvanize } from 'src/game/status/galvanize/constants';
 
 /*
 TODO:
 
 -for both cards
     -verify card type
-    -cards effects logic (All attack actions grant 4 defense this turn.)
+    -cards effects logic (All attack actions grant 3 defense this turn.)
 */
 
 export const GalvanizeCardUpgraded: Card = {
@@ -17,15 +19,16 @@ export const GalvanizeCardUpgraded: Card = {
     cardType: CardTypeEnum.Defend,
     pool: 'knight',
     energy: 1,
-    description: `All attack actions grant 4 ${defenseEffect.name} this turn.`,
+    description: `All attack actions grant 3 ${defenseEffect.name} this turn.`,
     keywords: [],
     properties: {
         effects: [
             {
-                effect: defenseEffect.name,
+                effect: attachStatusEffect.name,
                 target: CardTargetedEnum.Self,
                 args: {
-                    value: 4,
+                    statusName: galvanize.name,
+                    statusArgs: {},
                 },
             }
         ],
