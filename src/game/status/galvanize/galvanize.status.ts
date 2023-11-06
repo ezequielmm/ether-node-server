b2cf0d7627686dca14ac2d4e28c65548c20b4ef0
Martin Roux <mroux@kreitech.com.uy>
2023-11-06 12:11:26 -0300

    galvanize fix

--- src/game/status/galvanize/galvanize.status.ts
+++ src/game/status/galvanize/galvanize.status.ts
@@ -5,34 +5,72 @@ import { StatusDecorator } from '../status.decorator';
 import { galvanize } from './constants';
 import { CardTargetedEnum } from 'src/game/components/card/card.enum';
 import { defenseEffect } from 'src/game/effects/defense/constants';
+import { EVENT_BEFORE_PLAYER_TURN_END } from 'src/game/constants';
+import { OnEvent } from '@nestjs/event-emitter';
+import { GameContext } from 'src/game/components/interfaces';
+import { PlayerService } from 'src/game/components/player/player.service';
+import { StatusService } from '../status.service';
+import { CombatQueueService } from 'src/game/components/combatQueue/combatQueue.service';
+import { CombatQueueTargetEffectTypeEnum } from 'src/game/components/combatQueue/combatQueue.enum';
 
 @StatusDecorator({
     status: galvanize,
 })
 @Injectable()
 export class GalvanizeStatus implements StatusEventHandler {
-    constructor(private readonly effectService: EffectService) {}
+    constructor(
+        private readonly effectService: EffectService,
+        private readonly playerService: PlayerService,
+        private readonly statusService: StatusService,
+        private readonly combatQueueService: CombatQueueService
+    ){}
 
     async handle(dto: StatusEventDTO): Promise<void> {
 
-        console.log(dto);
+        const { ctx, source, target } = dto;
         
         if(dto.eventArgs.card.cardType == 'attack' && dto.source.type == 'player'){
 
-            console.log("entre al if cuando jugue ATTACK-------------------------");
-            
-            await this.effectService.apply({
-                ctx: dto.ctx,
-                source: dto.source,
-                target: dto.source,
-                effect: {
-                    effect: defenseEffect.name,
-                    args: {
-                        value: dto.status.args.value,
-                    },
+            const originalDefense = dto.source.value.combatState.defense; 
+            const defenseCalculated = originalDefense + dto.status.args.value;
+
+            await this.playerService.setDefense(dto.ctx, defenseCalculated);
+
+            await this.combatQueueService.push({
+                ctx,
+                source,
+                target,
+                args: {
+                    effectType: CombatQueueTargetEffectTypeEnum.Defense,
+                    defenseDelta: dto.status.args.value,
+                    finalDefense: defenseCalculated,
+                    healthDelta: 0,
+                    finalHealth: 0,
+                    statuses: [],
+                },
+                action:{
+                    name: 'defense',
+                    hint: 'defense'
                 },
             });
         }
+    }
+
+    @OnEvent(EVENT_BEFORE_PLAYER_TURN_END)
+    async onPlayerTurnStart({ ctx }: { ctx: GameContext }): Promise<void> {
+        const player = this.playerService.get(ctx);
+        const {
+            value: {
+                combatState: { statuses },
+            },
+        } = player;
+
+        await this.statusService.decreaseCounterAndRemove(
+            ctx,
+            statuses,
+            player,
+            galvanize,
+        );
     }
 
 }
