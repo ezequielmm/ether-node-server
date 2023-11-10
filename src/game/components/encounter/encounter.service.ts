4bbdd7767618aec98a4982e229d98d80aca3a409
ezequielmm <ezequiel@manufactory.gg>
2023-11-10 13:31:07 -0300

    test

--- src/game/components/encounter/encounter.service.ts
+++ src/game/components/encounter/encounter.service.ts
@@ -602,7 +602,7 @@ export class EncounterService {
         const probabilityWeights: number[] = [];
 
         for (const card of playerState.cards) {
-            if (!card.isUpgraded && card.upgradedCardId && card.cardType == CardTypeEnum.Attack) {
+            if (!card.isUpgraded && card.upgradedCardId) {
                 cardIds.push(card.cardId);
                 probabilityWeights.push(1);
             }
