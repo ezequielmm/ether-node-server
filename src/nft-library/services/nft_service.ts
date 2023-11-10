da7c6c5a08480eed12b9435c83cdc4c1dbd02c29
Camilo <corquera@kreitech.com.uy>
2023-11-10 14:49:04 +0100

    clean some code

--- src/nft-library/services/nft_service.ts
+++ src/nft-library/services/nft_service.ts
@@ -1,16 +1,12 @@
-import { forEach } from 'lodash';
 import { AlchemyService } from './alchemy_service';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { OwnedNft } from 'alchemy-sdk';
 
 @Injectable()
 export class NFTService {
 
-    constructor(private readonly alchemyService: AlchemyService, 
-                private readonly configService: ConfigService){}
+    constructor(private readonly alchemyService: AlchemyService){}
 
-    async isTokenIdFromWallet(contract:string, tokenId: number, walletAddress: string): Promise<boolean>{
+    public async isTokenIdFromWallet(contract:string, tokenId: number, walletAddress: string): Promise<boolean>{
         let NFTOwnedByWallet = false;
         const owners = await this.getOwnersForTokenAndContract(contract, tokenId);
 
@@ -24,77 +20,12 @@ export class NFTService {
         return NFTOwnedByWallet;
     }
 
-    async getOwnersForTokenAndContract(contract: string, tokenId: number): Promise<string[]> {
+    private async getOwnersForTokenAndContract(contract: string, tokenId: number): Promise<string[]> {
         try{
             return (await this.alchemyService.getInstance().nft.getOwnersForNft(contract, tokenId)).owners;
         }catch(error) {
             console.error("Error connecting to alchemy api:", error.message);
             return Promise.resolve([]);
-        }
-    }
-
-    async listByContracts({walletAddress, tokenAddresses, amount}: {walletAddress: string, tokenAddresses?: string[], amount}): Promise<any> 
-    {
-        // prep a collector for tokens by address
-        let tokenCollections: { 
-            [id: string] : { 
-                contract_address: string; 
-                token_count: number; 
-                tokens: any[]
-            }
-        } = {};
-        
-        // ensure all token addresses exist for pushing tokens
-        forEach(tokenAddresses, (address) => {
-            tokenCollections[address.toLowerCase()] = {
-                contract_address: address.toLowerCase(),
-                token_count: 0,
-                tokens: []
-            };
-        });
-
-
-        const alchemySettings = this.alchemyService.getInstance();
-        const pageSize = amount <= 100 ? amount : 100;
-
-        for(let address of tokenAddresses){
-            const nftsByCharacter = await alchemySettings.nft.getNftsForOwner(walletAddress, { pageSize, contractAddresses: [address] });
-            this.loadNftsByCharacterType(tokenCollections, address.toLowerCase(), nftsByCharacter.ownedNfts,  amount);
-        }
-
-        // return collections in an order matching the provided tokenAddresses
-        const collections = [];
-        forEach(tokenAddresses, (address) => {
-            collections.push(tokenCollections[address.toLowerCase()]);
-        });
-
-        return {
-            wallet: walletAddress,
-            tokens: collections
-        };
-        
-    }
-
-
-    private loadNftsByCharacterType(tokenCollections, address:string, ownedNFTS:OwnedNft[],  amount: number){
-        for(let nft of ownedNFTS){
-            tokenCollections[address].token_count = 1 + tokenCollections[address].token_count;
-            tokenCollections[address].tokens.push({
-                token_id: nft.tokenId,
-                amount: nft.balance, 
-                owner_of: "owner",
-                contract_type: nft.contract.tokenType,
-                name: nft.title,
-                symbol: nft.contract.symbol,
-                token_uri: nft.tokenUri,
-                last_token_uri_sync: "yesterday",
-                last_metadata_sync: "yesterday",
-                metadata: nft.rawMetadata,
-            });
-
-            if(tokenCollections[address].token_count >= amount){
-                break;
-            }
         }
     }
 
