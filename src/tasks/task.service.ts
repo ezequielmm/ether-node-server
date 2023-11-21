c0ea22057d00b64ebbeb2f59e1ff08f98e7e60ef
Camilo <corquera@kreitech.com.uy>
2023-11-21 17:08:50 +0100

    deactivated gear drops

--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -26,7 +26,7 @@ export class TaskService {
         private readonly settingsService: SettingsService,
     ) {}
 
-    @Cron(CronExpression.EVERY_DAY_AT_4PM, { 
+    @Cron(CronExpression.EVERY_DAY_AT_1PM, { 
         name: 'Create Contest Map',
         timeZone: 'UTC',
     })
