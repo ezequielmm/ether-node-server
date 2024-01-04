6b44e1b2ffb89de6b3fb8f6c07c559ef443689fd
Ezequiel Mora <ezequiel@manufactory.gg>
2024-01-04 11:57:11 -0300

    change to 3 seconds

--- src/game/status/dodge/dodge.status.ts
+++ src/game/status/dodge/dodge.status.ts
@@ -60,7 +60,7 @@ export class DodgeStatus implements StatusEffectHandler {
         return new Promise<void>((resolve) => {
           setTimeout(() => {
             resolve();
-          }, 7000); 
+          }, 3000); 
         });
       }
 }
