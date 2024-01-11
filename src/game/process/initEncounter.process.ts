1434d7ccfd9e97f1d6fb6467777c02da4bf5ba3f
Martin Roux <mroux@kreitech.com.uy>
2024-01-11 16:33:41 -0300

    first test

--- src/game/process/initEncounter.process.ts
+++ src/game/process/initEncounter.process.ts
@@ -41,7 +41,8 @@ export class InitEncounterProcess {
                 ctx,
                 this.node,
             );
-
+        
+        console.log('node data', currentNode);
         ctx.expedition.currentNode = currentNode;
         await ctx.expedition.save();
 
